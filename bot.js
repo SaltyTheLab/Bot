588bb59578446b9f10a196c81c785818337b9b09
Michael <michaelvargo@outlook.com>
2025-07-10 16:51:59 -0500

    add welcome and leave detection.

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@
 import fs from 'node:fs';
 import path from 'node:path';
 import { fileURLToPath, pathToFileURL } from 'url';
-import { Client, GatewayIntentBits, Collection } from 'discord.js';
+import { Client, GatewayIntentBits, Collection, EmbedBuilder } from 'discord.js';
 import { config } from 'dotenv';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -52,6 +52,7 @@ client.on('messageCreate', message => {
     }
 })
 
+
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
 });
@@ -66,5 +67,36 @@ client.on('messageCreate', message => {
     if (message.content.toLowerCase() == 'adorable')
         message.reply('You\'re Adorable')
 });
+
+client.on('guildMemberAdd', async (member) => {
+    const WelcomeEmbed = new EmbedBuilder()
+        .setColor(0x00FF99)
+        .setDescription(
+            `Welcome ${member} to the server!`
+        )
+        .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
+        .addFields(
+            { name: 'Discord Join Date:', value: `\`${member.joinedAt}\``, inline: true }
+        )
+    const welcomechannelid = '1392972733704572959';
+    const welcomeChannel = member.guild.channels.cache.get(welcomechannelid)
+    if (welcomeChannel) {
+        await welcomeChannel.send({ embeds: [WelcomeEmbed] })
+    } else {
+        console.warn('I can not find my welcome logs.')
+    }
+})
+
+client.on('guildMemberRemove', async (member) => {
+    const LeaveEmbed = new EmbedBuilder()
+        .setColor(0xa90000)
+        .setDescription(
+            `${member} has left the cave.`
+
+        )
+        .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
+        .addFields({ name: 'Joined the cave on:', value: `${member.guild.joinedAt()}`, inline: true })
+}
+)
 
 client.login(process.env.TOKEN);
