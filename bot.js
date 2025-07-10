3757decb06107cca9ecf85cb2a386b233f7d97cb
Michael <michaelvargo@outlook.com>
2025-07-09 20:05:29 -0500

    intiial setup

---
+++ bot.js
@@ -0,0 +1,11 @@
+import { Client, GatewayIntentBits } from 'discord.js';
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once('ready', () => {
+    console.log('Logged in as ${client.user.tag}');
+});
+client.on('messageCreate', message => { 
+    if (message.content == '!ping')
+message.reply('Pong!');})
+    
+client.login(process.env.TOKEN);
