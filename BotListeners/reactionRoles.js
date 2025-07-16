e3ffa702f0d94d8f858e8212f6e643b7dd61db2a
Michael <michaelvargo@outlook.com>
2025-07-16 07:10:12 -0500

    create listeners into functions.

--- BotListeners/reactionRoles.js
+++ BotListeners/reactionRoles.js
@@ -1,46 +1,34 @@
-const {Client, GatewayIntentBits, Partials } = require('discord.js');
 
-const client = Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.GuildMessageReactions,
-    GatewayIntentBits.MessageContent
-    ],
-  partials: [Partials.Message, Partials.Channel, Partials.Reaction].
-    });
-
-client.once('ready', () => {
-  console.log(`Logged in as $(client.user.tag}`);
-});
 
 const roleMessageId = ''; //Insert message ID here
-const emojiRoleMap = { 
+const emojiRoleMap = {
   '': '', //Insert emoji in first apostrophe role ID in second here
 };
 
-client.on('messageReactionAdd', async (reaction, user) => {
-  if (reaction.message.id !== roleMessageId || user.bot) return;
+export async function messageReactionAdd(reaction, user) {
 
-  const roleID = emojiRoleMap[reaaction.emoji.name];
+  if (reaction.message.id !== roleMessageId || user.bot) return;
+ 
+
+  const roleID = emojiRoleMap[reaction.emoji.name];
   const guild = reaction.message.guild;
   const member = await guild.members.fetch(user.id);
 
-  if(roleID) {
+  if (roleID) {
     member.roles.add(roleID).catch(console.error);
   }
-});
+}
 
-client.on('messageReactionRemove' async (reactiom. user) => {
+export async function messageReactionRemove(reaction, user) {
   if (reaction.message.id !== roleMessageId || user.bot) return;
 
-  const roleID = emojiRoleMap[reaaction.emoji.name];
+  const roleID = emojiRoleMap[reaction.emoji.name];
   const guild = reaction.message.guild;
   const member = await guild.members.fetch(user.id);
 
-  if(roleID) {
-    member.roles.add(roleID).catch(console.error);
+  if (roleID) {
+    member.roles.remove(roleID).catch(console.error);
   }
-});
+};
 
-client.login(process.env.TOKEN);
+
