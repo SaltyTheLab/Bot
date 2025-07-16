abcc66326321c62709759afbf69b64cfebbb0db2
Michael <michaelvargo@outlook.com>
2025-07-16 15:07:12 -0500

    helper function for storing message ids.

---
+++ utilities/messageStorage.js
@@ -0,0 +1,19 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filePath = path.join(__dirname, '../reactionRoleMessages.json');
+
+export function loadMessageIDs() {
+    if (fs.existsSync(filePath)) {
+        return JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+    }
+    return {};
+}
+
+export function saveMessageIDs(data) {
+    fs.writeFileSync(filePath, JSON.stringify(data, null, 4), 'utf-8');
+}
