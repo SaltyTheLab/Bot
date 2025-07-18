01ba43d8ec72d2e456e7ceeef033347fb0f62e59
Michael <michaelvargo@outlook.com>
2025-07-18 18:07:17 -0500

    small changes.

--- moderation/punishments.js
+++ moderation/punishments.js
@@ -1,12 +1,14 @@
-export function getNextPunishment(warningCount) {
+export function getNextPunishment(weightedWarns) {
     const punishments = [
         '1 Warn',
         '15 min mute',
         '30 min mute',
+        '45 min mute',
         '1 hour mute',
         '2 hour mute',
         '4 hour mute',
-        '6 hour mute',
+        '6 hour mute'
     ]
-      return punishments[Math.min(warningCount, punishments.length - 1)];
+      const index =  Math.min(Math.floor(weightedWarns), punishments.length);
+      return punishments[index];
 }
