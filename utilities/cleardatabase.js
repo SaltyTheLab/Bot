522174424f711abbec131e918f825d738afa677e
Michael <michaelvargo@outlook.com>
2025-07-19 12:45:09 -0500

    clear specific users instead of full database.

--- utilities/cleardatabase.js
+++ utilities/cleardatabase.js
@@ -1,42 +1,11 @@
 import sqlite3 from 'sqlite3'
 import { open } from 'sqlite'
-export async function resetModerationTables() {
-  const dbPromise = open({
+export async function clearmodlogs(userId) {
+  const db = open({
     filename: './Logging/database.sqlite',
     driver: sqlite3.Database
   });
-
-  dbPromise.then(async db => {
-
-    db.exec(`   
-      DROP TABLE IF EXISTS warns;
-        DROP TABLE IF EXISTS mutes;
-  
-
-      CREATE TABLE IF NOT EXISTS warns (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      userId TEXT NOT NULL,
-      moderatorId TEXT NOT NULL,
-      reason TEXT NOT NULL,
-      timestamp INTEGER NOT NULL,
-      active INTEGER DEFAULT 1,
-      weight INTEGER DEFAULT 1,
-      type TEXT
-    );
-    CREATE TABLE IF NOT EXISTS mutes (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      userId TEXT NOT NULL,
-      moderatorId TEXT NOT NULL,
-      reason TEXT NOT NULL,
-      duration INTEGER NOT NULL,
-      timestamp INTEGER NOT NULL,
-      active DEFAULT 1,
-      weight INTEGER DEFAULT 1,
-      type TEXT
-    );
-
-    `)
-  });
-
-  console.log('✅ Moderation tables reset.');
+  db.run(`DELETE FROM warns WHERE userId = ?`, userId);
+  db.run(`DELETE FROM mutes WHERE userId = ?`, userId);
+  console.log(`✅ Cleared moderation tables for user: ${userId} `);
 }
