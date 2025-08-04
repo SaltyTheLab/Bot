6c9ead9722e22a4862448f1e83b111d380a264e1
Michael <michaelvargo@outlook.com>
2025-08-03 19:57:17 -0500

    format for clarity

--- Database/Schema.sql
+++ Database/Schema.sql
@@ -1,35 +1,37 @@
+PRAGMA foreign_keys = ON;
 
-CREATE TABLE IF NOT EXISTS punishments (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    userId TEXT  NOT NULL,
-    moderatorId TEXT NOT NULL,
-    reason TEXT NOT NULL,
-    type TEXT NOT NULL,
-    timestamp INTEGER NOT NULL,
-    duration INTEGER,
-    active INTEGER DEFAULT 1,
-    weight INTEGER DEFAULT 1,
-    channel TEXT NOT NULL,
-    FOREIGN KEY (userId) REFERENCES users(userId)
-);
+PRAGMA journal_mode = DELETE;
 
-CREATE TABLE IF NOT EXISTS users (
-    userId TEXT,
-    xp INTEGER DEFAULT 0,
-    level INTEGER DEFAULT 1,
-    coins INTEGER DEFAULT 100,
-    PRIMARY KEY (userId)
-);
+CREATE TABLE
+    IF NOT EXISTS punishments (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        userId TEXT NOT NULL,
+        moderatorId TEXT NOT NULL,
+        reason TEXT NOT NULL,
+        type TEXT NOT NULL,
+        timestamp INTEGER NOT NULL,
+        duration INTEGER,
+        active INTEGER DEFAULT 1,
+        weight INTEGER DEFAULT 1,
+        channel TEXT NOT NULL,
+        FOREIGN KEY (userId) REFERENCES users (userId)
+    );
 
-CREATE TABLE IF NOT EXISTS notes (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    userId TEXT NOT NULL,
-    moderatorId TEXT NOT NULL,
-    note TEXT NOT NULL,
-    timestamp INTEGER NOT NULL,
-    FOREIGN KEY (userId) REFERENCES users(userId)
-);
+CREATE TABLE
+    IF NOT EXISTS users (
+        userId TEXT,
+        xp INTEGER DEFAULT 0,
+        level INTEGER DEFAULT 1,
+        coins INTEGER DEFAULT 100,
+        PRIMARY KEY (userId)
+    );
 
-DROP INDEX IF EXISTS idx_users_userId;
-DROP INDEX IF EXISTS idx_punishments_user_type;
-DROP INDEX IF EXISTS idx_punishments_user_active;
+CREATE TABLE
+    IF NOT EXISTS notes (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        userId TEXT NOT NULL,
+        moderatorId TEXT NOT NULL,
+        note TEXT NOT NULL,
+        timestamp INTEGER NOT NULL,
+        FOREIGN KEY (userId) REFERENCES users (userId)
+    );
--- Database/database.js
+++ Database/database.js
@@ -6,21 +6,18 @@ export const db = new Database('./Database/database.sqlite', {
   fileMustExist: true,
 });
 
-db.pragma('foreign_keys = ON')
-db.pragma('journal_mode=DELETE')
 //load and execute schema from seperate sql file
 const schemaPath = path.resolve('./Database/Schema.sql');
 const schema = fs.readFileSync(schemaPath, 'utf-8');
 const statements = schema.split(';').filter(stmt => stmt.trim() !== '');
 //run the schema of the database
-for (const stmt of statements){
+for (const stmt of statements) {
   try {
     db.exec(stmt + ';');
-  }catch (error){
+  } catch (error) {
     console.error(`error exectuing schema statement: ${stmt.trim()}`);
     console.error(error);
     throw error;
   }
 }
-//export default open database for database functions
 export default db;
