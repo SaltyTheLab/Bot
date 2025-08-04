a00ed147956aa81ea9e03e7e68286c108c9cca2f
Michael <michaelvargo@outlook.com>
2025-08-03 20:03:54 -0500

    refactor action from ifs to a switch statement

--- BotListeners/guildMemberRemove.js
+++ BotListeners/guildMemberRemove.js
@@ -1,11 +1,11 @@
-import { EmbedBuilder, AuditLogEvent, Embed } from "discord.js";
-import { welcomeChannelId } from "./Extravariables/channelids.js";
-import { banlogChannelid } from "./Extravariables/channelids.js";
+import { EmbedBuilder, AuditLogEvent } from "discord.js";
+import { welcomeChannelId, banlogChannelid, mutelogChannelid } from "./Extravariables/channelids.js";
 
 export async function guildMemberRemove(member) {
     //define welcomechannel and banlog channel
-    const [welcomeChannel, banlogChannel] = [member.guild.channels.cache.get(welcomeChannelId),
-    member.guild.channels.cache.get(banlogChannelid)
+    const [welcomeChannel, banlogChannel, muteLogChannel] = [member.guild.channels.cache.get(welcomeChannelId),
+    member.guild.channels.cache.get(banlogChannelid),
+    member.guild.channels.cache.get(mutelogChannelid)
     ]
 
     if (!welcomeChannel) {
@@ -77,6 +77,7 @@ export async function guildMemberRemove(member) {
                 : 'Unknown',
             inline: true,
         })
+
     // Create leave message embed
     const embed = new EmbedBuilder()
         .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
@@ -86,41 +87,58 @@ export async function guildMemberRemove(member) {
                 ? `<t:${Math.floor(member.joinedAt.getTime() / 1000)}:F>`
                 : 'Unknown',
             inline: true,
-        });
-
+        })
+        .setFooter({ text: time });
     // Customize message by action
-    if (action === "ban") {
-        embed
-            .setColor(0x8b0000)
-            .setTitle('A member was banned')
-            .addFields(
-                { name: 'User', value: `<@${member.id}>`, inline: true },
-                { name: 'Banned by', value: `<@${executor.id}>`, inline: true },
-                { name: 'Reason', value: reason }
-            );
-        banlogChannel.send({ embeds: [embed] })
-    } else if (action === "kick") {
-        embed
-            .setColor(0xff5555)
-            .setTitle('A member was kicked')
-            .addFields(
-                { name: 'User', value: `<@${member.id}>`, inline: true },
-                { name: 'Kicked by', value: `<@${executor.id}>`, inline: true },
-                { name: 'Reason', value: reason }
-            );
-    } else if (action === "prune") {
-        embed
-            .setColor(0xffaa00)
-            .setTitle('A member was likely pruned')
-            .setDescription(`${member} was probably removed in a server prune.`);
-    } else {
-        embed
-            .setColor(0xa90000)
-            .setTitle('A member left the cave')
-            .setDescription(`${member} has left the server.`);
+    switch (action) {
+        case "ban":
+            embed
+                .setColor(0x8b0000)
+                .setTitle('A member was banned')
+                .addFields(
+                    { name: 'User', value: `<@${member.id}>`, inline: true },
+                    { name: 'Banned by', value: `<@${executor.id}>`, inline: true },
+                    { name: 'Reason', value: reason }
+                )
+                .setFooter({ text: time })
+            await banlogChannel.send({ embeds: [embed] });
+            break;
+
+        case "kick":
+            embed
+                .setColor(0xff5555)
+                .setTitle('A member was kicked')
+                .addFields(
+                    { name: 'User', value: `<@${member.id}>`, inline: true },
+                    { name: 'Kicked by', value: `<@${executor.id}>`, inline: true },
+                    { name: 'Reason', value: reason }
+                )
+                .setFooter({ text: time })
+            await muteLogChannel.send({ embeds: [embed] });
+            break;
+
+        case "prune":
+            embed
+                .setColor(0xffaa00)
+                .setTitle('A member was likely pruned')
+                .setDescription(`${member} was probably removed in a server prune.`)
+                .setFooter({ text: time });
+            await muteLogChannel.send({ embeds: [embed] });
+            break;
+
+        default:
+            embed
+                .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
+                .setDescription(`<@${member.id}> left the cave.`)
+                .addFields({
+                    name: 'Joined the cave on:',
+                    value: member.joinedAt
+                        ? `<t:${Math.floor(member.joinedAt.getTime() / 1000)}:F>`
+                        : 'Unknown',
+                    inline: true,
+                })
+                .setTimestamp()
+            break;
     }
-    //send embed
     await welcomeChannel.send({ embeds: [leaveembed] });
-    if (action === "ban")
-        await banlogChannel.send({ embeds: [embed] });
 }
