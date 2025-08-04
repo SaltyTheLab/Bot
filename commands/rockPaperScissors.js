88d891266d75d865b5eab88155b8a646381fe972
Michael <michaelvargo@outlook.com>
2025-08-03 19:58:21 -0500

    rename file for clarity and update database function imports

--- commands/rockPaperScissors.js
+++ commands/rockPaperScissors.js
@@ -1,5 +1,5 @@
 import { EmbedBuilder, SlashCommandBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } from "discord.js";
-import { getUser, saveUser } from "../Database/databasefunctions.js";
+import { getUser, saveUser } from "../Database/databaseFunctions.js";
 
 export const data = new SlashCommandBuilder()
     .setName('rps')
