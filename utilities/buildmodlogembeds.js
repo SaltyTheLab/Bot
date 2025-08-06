10e8b7928bea3037cf171ba7475a0dcce9f00380
Michael <michaelvargo@outlook.com>
2025-08-06 18:56:48 -0500

    rename file for clarity and combine similar methods.

--- utilities/buildmodlogembeds.js
+++ utilities/buildmodlogembeds.js
@@ -6,7 +6,8 @@ const LOG_COLORS = {
     Ban: 0xd1b1bf
 };
 //define and build embed template
-export async function buildLogEmbed(interaction, log, idx, totalLogs, targetUser) {
+export async function buildLogEmbed(interaction, log, idx, totalLogs) {
+    const targetUser = await interaction.client.users.fetch(log.userId);
     const moderator = await interaction.client.users.fetch(log.moderatorId);
     const formattedDate = new Date(log.timestamp).toLocaleString('en-US', {
         dateStyle: 'medium',
@@ -15,7 +16,7 @@ export async function buildLogEmbed(interaction, log, idx, totalLogs, targetUser
     });
 
     const fields = [
-        { name: 'Member', value: `<@${targetUser.id}>`, inline: true },
+        { name: 'Member', value: `<@${log.userId}>`, inline: true },
         { name: 'Type', value: `\`${log.type}\``, inline: true },
         { name: 'Channel', value: `<#${log.channel}>`, inline: false },
         { name: 'Reason', value: `\`${log.reason || 'No reason provided'}\``, inline: false },
@@ -45,15 +46,15 @@ export async function buildLogEmbed(interaction, log, idx, totalLogs, targetUser
             iconURL: moderator.displayAvatarURL({ dynamic: true })
         });
 };
-export async function buildButtons(idx, totalLogs, targetUserId, isDeletable, logId, logType, timestamp, disabled = false) {
+export async function buildButtons(idx, totalLogs, targetUserId, isDeletable, logId, logType, disabled = false) {
     const buttons = [
         new ButtonBuilder()
-            .setCustomId(`modlog_prev_${targetUserId}_${idx}_${timestamp}`)
+            .setCustomId(`modlog_prev_${targetUserId}_${idx}`)
             .setLabel('⬅️ Back')
             .setStyle(ButtonStyle.Secondary)
             .setDisabled(idx === 0 || disabled),
         new ButtonBuilder()
-            .setCustomId(`modlog_next_${targetUserId}_${idx}_${timestamp}`)
+            .setCustomId(`modlog_next_${targetUserId}_${idx}`)
             .setLabel('Next ➡️')
             .setStyle(ButtonStyle.Secondary)
             .setDisabled(idx >= totalLogs - 1 || disabled)
@@ -62,11 +63,54 @@ export async function buildButtons(idx, totalLogs, targetUserId, isDeletable, lo
     if (isDeletable) {
         buttons.push(
             new ButtonBuilder()
-                .setCustomId(`modlog_del_${targetUserId}_${logId}_${logType}_${idx}_${timestamp}`)
+                .setCustomId(`modlog_del_${targetUserId}_${logId}_${logType}_${idx}`)
                 .setLabel('Delete')
                 .setStyle(ButtonStyle.Danger)
                 .setDisabled(disabled)
         );
     }
     return new ActionRowBuilder().addComponents(...buttons);
+};
+export async function buildNoteEmbed(interaction, index, currentNote, length) {
+
+    const target = await interaction.client.users.fetch(currentNote.userId);
+    const mod = await interaction.client.users.fetch(currentNote.moderatorId);
+    const formattedDate = new Date(currentNote.timestamp).toLocaleString('en-US', {
+        dateStyle: 'medium',
+        timeStyle: 'short',
+        timeZone: 'CST'
+    });
+
+    return new EmbedBuilder()
+        .setColor(0xdddddd)
+        .setThumbnail(target.displayAvatarURL({ dynamic: true }))
+        .setDescription([
+            `${target} notes |  \`${index + 1} of ${length}\``,
+            `> ${currentNote.note}`
+        ].join('\n'))
+        .setFooter({
+            text: `${mod.tag} | ${formattedDate}`,
+            iconURL: mod.displayAvatarURL({ dynamic: true })
+        });
+};
+export async function buildNoteButtons(target, index, currentNote, allnotes, disabled = false) {
+    return new ActionRowBuilder().addComponents(
+        new ButtonBuilder()
+            .setCustomId(`note_prev_${target}_${index}`)
+            .setLabel('◀️ prev')
+            .setStyle(ButtonStyle.Secondary)
+            .setDisabled(index === 0 || disabled),
+
+        new ButtonBuilder()
+            .setCustomId(`note_next_${target}_${index}`)
+            .setLabel('▶️ next')
+            .setStyle(ButtonStyle.Secondary)
+            .setDisabled(index === allnotes - 1 || disabled),
+
+        new ButtonBuilder()
+            .setCustomId(`note_delete_${target}_${index}_${currentNote.id}`)
+            .setLabel('🗑️ delete')
+            .setStyle(ButtonStyle.Danger)
+            .setDisabled(disabled)
+    );
 };
