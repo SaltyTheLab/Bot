5369212f64f172ef8748babe378d951bf86e7551
Michael <michaelvargo@outlook.com>
2025-08-14 20:45:28 -0500

    add automatic invite tracking(still a wip)

---
+++ BotListeners/inviteCreate.js
@@ -0,0 +1,9 @@
+import invites from "./Extravariables/invites.js";
+export function inviteCreate(invite) {
+    const key = `${invite.guild.id}-${invite.code}`
+    invites.set(key, invite.uses)
+    console.log(`added invite: ${key} to the cache`)
+    invites.forEach((uses, key) => {
+        console.log(`${key}, Uses: ${uses}`);
+    })
+}
