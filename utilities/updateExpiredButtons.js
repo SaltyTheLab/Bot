5f1c27678ea649da012be1b63e67e5f5c5afb0ff
Michael <michaelvargo@outlook.com>
2025-08-17 14:05:01 -0500

    remove try and catch block

--- utilities/updateExpiredButtons.js
+++ utilities/updateExpiredButtons.js
@@ -1,38 +1,40 @@
 import { ActionRowBuilder, ButtonBuilder, ButtonStyle } from "discord.js";
 import { guildModChannelMap } from "../BotListeners/Extravariables/channelids.js";
 export default async function updateExpiredButtons(client, guildIds) {
-    const guildId = guildIds.split(',').map(id => id.trim());
-    for (const id of guildId) {
-        try {
-            const guild = await client.guilds.fetch(id);
-            const Channels = guildModChannelMap[id];
-            const welcomeChannel = await guild.channels.fetch(Channels.welcomeChannel);
-            if (!welcomeChannel) {
-                console.log(`Welcome Channel not found for ${guild.name} (${guildId})`);
-                return;
-            }
-            const now = Date.now();
-            const fiffteenMinutesAgo = now - 15 * 60 * 1000;
-            const messages = await welcomeChannel.messages.fetch({ limit: 50 });
-            for (const message of messages.values()) {
-                const messageTimestamp = message.createdTimestamp;
-                if (messageTimestamp > fiffteenMinutesAgo) {
-                    const buttonComponent = message.components[0];
-                    if (buttonComponent && !buttonComponent.disabled) {
-                        const updatedBanButton = new ButtonBuilder()
-                            .setCustomId(buttonComponent.components[0].customId)
-                            .setLabel(buttonComponent.components[0].label === '🔨 Ban User & Delete Invite' ? '🔨 Ban User & Delete Invite (Expired)' : '🔨 Ban (Expired)')
-                            .setStyle(ButtonStyle.Danger)
-                            .setDisabled(true)
+    const guildIdarray = guildIds.split(',').map(id => id.trim());
+    const guildFetchPromises = guildIdarray.map(id => client.guilds.fetch(id));
+    const guilds = await Promise.all(guildFetchPromises);
 
-                        const updatedActionRow = new ActionRowBuilder().addComponents(updatedBanButton);
-                        await message.edit({ components: [updatedActionRow] });
-                        console.log(`Ban button in message ${message.id} disabled.`);
-                    }
+    for (const guild of guilds) {
+
+        const Channels = guildModChannelMap[guild.id];
+        const welcomeChannel = await guild.channels.fetch(Channels.welcomeChannel);
+        if (!welcomeChannel) {
+            console.log(`Welcome Channel not found for ${guild.name} (${guild.id})`);
+            return;
+        }
+        const now = Date.now();
+        const fiffteenMinutesAgo = now - 15 * 60 * 1000;
+        const messages = await welcomeChannel.messages.fetch({ limit: 50 });
+
+        for (const message of messages.values()) {
+            const messageTimestamp = message.createdTimestamp;
+
+            if (messageTimestamp > fiffteenMinutesAgo) {
+                const buttonComponent = message.components[0];
+                if (buttonComponent && !buttonComponent.disabled) {
+                    const updatedBanButton = new ButtonBuilder()
+                        .setCustomId(buttonComponent.components[0].customId)
+                        .setLabel(buttonComponent.components[0].label === '🔨 Ban User & Delete Invite' ? '🔨 Ban User & Delete Invite (Expired)'
+                            : '🔨 Ban (Expired)')
+                        .setStyle(ButtonStyle.Danger)
+                        .setDisabled(true)
+
+                    const updatedActionRow = new ActionRowBuilder().addComponents(updatedBanButton);
+                    await message.edit({ components: [updatedActionRow] });
+                    console.log(`Ban button in message ${message.id} disabled.`);
                 }
             }
-        } catch (error) {
-            console.error(`Error updating expired buttons for guild ${guildId}:`, error)
         }
     }
 }
