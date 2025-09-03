8ece3cfe640d537bf241e8e1ad6104a179fdf7d9
Michael <michaelvargo@outlook.com>
2025-09-03 11:44:05 -0500

    remove unused command and add unwarn command

---
+++ commands/unwarn.js
@@ -0,0 +1,24 @@
+import { SlashCommandBuilder, EmbedBuilder, InteractionContextType, PermissionFlagsBits } from "discord.js";
+import { unwarn } from "../Database/databasefunctions.js";
+
+export const data = new SlashCommandBuilder()
+    .setName('unwarn')
+    .setDescription('removes a users most recent warn')
+    .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers)
+    .setContexts(InteractionContextType.Guild)
+    .addUserOption(opt =>
+        opt.setName('target').setDescription('User to remove warn from').setRequired(true)
+    )
+
+export async function execute(interaction) {
+    const user = interaction.options.getUser('target')
+    const guildId = interaction.guild.id;
+    await unwarn(user.id, guildId);
+
+    const commandembed = new EmbedBuilder()
+        .setDescription(`recent warn removed from${user}`)
+        .setColor(0x007800)
+    interaction.reply({
+        embeds: [commandembed]
+    })
+}
