4d4648bf85ca1b0ac20ea061c0298058943c5e8c
Michael <michaelvargo@outlook.com>
2025-10-01 00:48:11 -0500

    add computer

--- commands/tictactoe.js
+++ commands/tictactoe.js
@@ -54,25 +54,61 @@ function checkWinner(gameBoard, player) {
 function checkDraw(gameBoard) {
     return gameBoard.every(cell => cell !== ' ')
 }
+
+function getCPUMove(gameBoard) {
+    // 1. Find all available (empty) moves
+    const availableMoves = gameBoard
+        .map((cell, index) => (cell === ' ' ? index : null))
+        .filter(index => index !== null);
+
+    // 2. Pick a random index from the available moves array
+    if (availableMoves.length > 0) {
+        const randomIndex = Math.floor(Math.random() * availableMoves.length);
+        return availableMoves[randomIndex];
+    }
+
+    return -1; // Should not happen in a non-drawn game
+}
+
 export const data = new SlashCommandBuilder()
     .setName('tictactoe')
     .setDescription('Play a game of TicTacToe')
-    .addUserOption(opt =>
-        opt.setName('opponent').setDescription('Choose your Opponent').setRequired(true)
+    .addSubcommand(subcommand =>
+        subcommand.setName('user').setDescription('Play against another person').addUserOption(option =>
+            option.setName('opponent').setDescription('The user you want to play against').setRequired(true)
+        )
+    )
+    .addSubcommand(subcommand =>
+        subcommand.setName('cpu').setDescription('Play against the cpu')
     )
     .setContexts(InteractionContextType.Guild)
 
 export async function execute(interaction) {
-    const opponent = interaction.options.getUser('opponent')
+    const subcommand = interaction.options.getSubcommand()
     const player1 = interaction.user;
-    const player2 = opponent;
+    let player2;
+    let isCPU = false;
 
-    if (player1.id === player2.id) {
-        return interaction.reply({
-            content: "You can't play against yourself.",
-            ephemeral: true
-        })
+    if (subcommand === 'cpu') {
+        player2 = {
+            id: 'CPU',
+            username: 'Computer',
+            toString: () => 'The **Computer**',
+            displayAvatarURL: () => 'https://i.imgur.com/8Xo9Q9Q.png'
+        };
+        isCPU = true
+    } else if (subcommand === 'user') {
+        const opponent = interaction.options.getUser('opponent');
+        if (player1.id === player2.id) {
+            return interaction.reply({
+                content: "You can't play against yourself.",
+                ephemeral: true
+            })
+        }
+        player2 = opponent;
     }
+
+
 
     let currentplayer = Math.random() < 0.5 ? player1 : player2;
 
@@ -80,6 +116,7 @@ export async function execute(interaction) {
     const players = new Map();
     players.set(player1.id, 'X');
     players.set(player2.id, 'O');
+    const player2Mark = 'O'
 
     const sentMessage = await interaction.reply({
         embeds: [generateEmbed(`It's ${currentplayer}'s turn to move `)],
@@ -93,6 +130,39 @@ export async function execute(interaction) {
         time: 120000
     })
 
+    async function cpuMoveHandler() {
+        const cpuMove = getCPUMove(gameBoard);
+
+        if (cpuMove !== -1) {
+            gameBoard[cpuMove] = player2Mark;
+            if (checkWinner(gameBoard, player2Mark)) {
+                return interaction.editReply({
+                    embeds: [generateEmbed(`${player2.toString()} wins!!`, "Gold")],
+                    components: generateButtons(gameBoard, true)
+                });
+            }
+
+
+            if (checkDraw(gameBoard)) {
+                collector.stop('draw');
+                return interaction.editReply({
+                    embeds: [generateEmbed(`It's a Draw!`, "Grey")],
+                    components: generateButtons(gameBoard, true)
+                });
+            }
+
+            currentplayer = player1;
+            await interaction.editReply({
+                embeds: [generateEmbed(`It's ${currentplayer.toString()}'s turn to move.`)],
+                components: generateButtons(gameBoard)
+            });
+        }
+    }
+
+    if (isCPU && currentplayer.id === player2.id) {
+        await cpuMoveHandler();
+    }
+
     collector.on('collect', async i => {
         if (i.user.id !== player1.id && i.user.id !== player2.id) {
             return i.reply({ content: 'You are not a participant in this game.', ephemeral: true })
@@ -105,13 +175,13 @@ export async function execute(interaction) {
         }
 
         const move = parseInt(i.customId.split('-')[1]);
-
-        if (isNaN(move)) {
-            console.error("Error: The parsed move value is NaN. Custom ID was:", i.customId);
-            return i.reply({ content: "There was an issue processing your move. Please try again.", ephemeral: true });
-        }
-
         const playerMark = players.get(currentplayer.id);
+
+        if (isNaN(move) || gameBoard[move] !== ' ') {
+            // Defer the update to prevent 'Interaction Failed' while waiting for the CPU
+            await i.deferUpdate();
+            return;
+        }
 
         if (gameBoard[move] !== ' ') {
             console.log(`Debug: User ${i.user.username} clicked button ${move}. The spot is already taken by ${gameBoard[move]}.`);
@@ -142,10 +212,17 @@ export async function execute(interaction) {
         }
 
         currentplayer = (currentplayer.id === player1.id) ? player2 : player1;
-        await i.update({
-            embeds: [generateEmbed(`It's ${currentplayer}'s turn to move.`)],
-            components: generateButtons(gameBoard)
-        })
+        if (isCPU) {
+            await i.update({
+                embeds: [generateEmbed(`It's ${currentplayer.toString()}'s turn to move.`)],
+                components: generateButtons(gameBoard)
+            })
+            setTimeout(() => cpuMoveHandler(), 750);
+        } else
+            await i.update({
+                embeds: [generateEmbed(`It's ${currentplayer}'s turn to move.`)],
+                components: generateButtons(gameBoard)
+            })
     });
 
     collector.on('end', (collected, reason) => {
