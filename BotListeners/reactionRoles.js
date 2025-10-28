7b7c463266b96ffcb221d8d690d474f36b2b1ef9
Michael <michaelvargo@outlook.com>
2025-10-27 20:08:35 -0500

    clean up code

--- BotListeners/reactionRoles.js
+++ BotListeners/reactionRoles.js
@@ -1,16 +1,12 @@
-import embedIDs from '../embeds/embedIDs.json' with {type: 'json'}
-import guildconfig from "./Extravariables/guildconfiguration.json" with {type: 'json'};
+import guildChannelMap from "./Extravariables/guildconfiguration.json" with {type: 'json'};
 import { getblacklist } from '../Database/databasefunctions.js';
-/**
- * Handle reaction-based role assignment or removal
- */
+import { load } from '../utilities/jsonloaders.js';
 async function handleReactionChange(reaction, user, action = 'add') {
-  //check for bot user
+  const embedIDs = await load("embeds/EmbedIDs.json");
   if (user.bot) return;
-
-  const reactions = guildconfig[reaction.message.guild.id].reactions;
+  const reactions = guildChannelMap[reaction.message.guild.id].reactions;
   const member = await reaction.message.guild.members.fetch(user.id);
-  //fetch the reaction the user used
+
   try {
     if (reaction.partial) reaction = await reaction.fetch();
     if (reaction.message && reaction.message.partial) await reaction.message.fetch();
@@ -20,31 +16,19 @@ async function handleReactionChange(reaction, user, action = 'add') {
     return;
   }
   const guildEmbeds = embedIDs[reaction.message.guild.id];
-  //return if not a vaild message or message id isn't in the messageIDs array
   const isValidMessageId = guildEmbeds.some(embedInfo => embedInfo.messageId === reaction.message.id);
 
   if (!isValidMessageId)
     return;
 
-  //assign the emoji id and role
   const emoji = reaction.emoji.id || reaction.emoji.name;
   const roleID = reactions[emoji];
 
-  //error out if role id not found
-  if (!roleID) {
-    console.log(`⚠️ No role mapped to emoji: ${emoji}`);
-    return;
-  }
-  let blacklist;
-  try {
-    blacklist = await getblacklist(user.id, reaction.message.guild.id)
-  } catch {
-    return;
-  }
+  if (!roleID) { console.log(`⚠️ No role mapped to emoji: ${emoji}`); return; }
 
-  // attempt to modify the users roles
-  if (blacklist.find(r => r === roleID))
+  if (await getblacklist(user.id, reaction.message.guild.id).length > 0 && await getblacklist(user.id, reaction.message.guild.id).find(r => r === roleID))
     return;
+
   try {
     const rolesToModify = Array.isArray(roleID) ? roleID : [roleID];
     await member.roles[action](rolesToModify);
@@ -52,13 +36,9 @@ async function handleReactionChange(reaction, user, action = 'add') {
     console.error(`❌ Failed to ${action} role(s):`, err);
   }
 }
-
-// Reaction add handler
 export async function messageReactionAdd(reaction, user) {
   await handleReactionChange(reaction, user, 'add');
 }
-
-// Reaction remove handler
 export async function messageReactionRemove(reaction, user) {
   await handleReactionChange(reaction, user, 'remove');
 }
