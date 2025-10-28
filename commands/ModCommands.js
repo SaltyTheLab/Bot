305d01ad1ea562b80e59c7547f634dfc6756ce93
Michael <michaelvargo@outlook.com>
2025-10-27 20:16:47 -0500

    remove time calculation for mute since it was moved to punish user

--- commands/ModCommands.js
+++ commands/ModCommands.js
@@ -1,8 +1,5 @@
 import { InteractionContextType, PermissionFlagsBits, SlashCommandBuilder, EmbedBuilder, GuildMember } from "discord.js";
-import { getActiveWarns } from "../Database/databasefunctions.js";
-import getNextPunishment from "../moderation/punishments.js";
 import punishUser from "../moderation/punishUser.js";
-const unitMap = { min: 60000, hour: 3600000, day: 86400000 };
 export const data = new SlashCommandBuilder()
     .setName('member')
     .setDescription('Warn/Mute/Ban a member')
@@ -51,11 +48,7 @@ export async function execute(interaction) {
     const target = await interaction.options.getMember('target') ?? await interaction.options.getUser('target');
     const reason = interaction.options.getString('reason');
     const staffcheck = target instanceof GuildMember ? target.permissions.has(PermissionFlagsBits.ModerateMembers) : null
-    let duration;
-    let unit = 'min';
-    let durationMs;
     let banflag = false
-    const MAX_TIMEOUT_MS = 2419200000;
     if (target.bot)
         return interaction.reply({ content: 'You cannot ban a bot.', ephemeral: true });
 
@@ -76,39 +69,21 @@ export async function execute(interaction) {
     }
 
     switch (command) {
-        case 'warn': {
-            const warns = await getActiveWarns(target.id, interaction.guild.id)
-            if (warns.length >= 2) {
-                ({ duration, unit } = getNextPunishment(warns.length))
-                const multiplier = unitMap[unit];
-
-                durationMs = duration * multiplier;
-                durationMs = Math.min(durationMs, MAX_TIMEOUT_MS);
-            }
-            break;
-        }
         case 'mute': {
-            duration = interaction.options.getInteger('duration')
-            unit = interaction.options.getString('unit')
-            const multiplier = unitMap[unit];
-            if (duration <= 0) {
+            const duration = interaction.options.getInteger('duration')
+            if (duration <= 0)
                 return interaction.reply({ content: '❌ Invalid duration or unit.', ephemeral: true });
-            }
-
-            durationMs = duration * multiplier;
-            durationMs = Math.min(durationMs, MAX_TIMEOUT_MS);
-
-            if (target.communicationDisabledUntilTimestamp && target.communicationDisabledUntilTimestamp > Date.now()) {
+            if (target.communicationDisabledUntilTimestamp && target.communicationDisabledUntilTimestamp > Date.now())
                 return interaction.reply({ content: '⚠️ User is already muted.', ephemeral: true });
-            }
             break;
         }
         case 'ban': {
-            if (interaction.member.permissions.has(PermissionFlagsBits.BanMembers))
+            if (interaction.member.permissions.has(PermissionFlagsBits.BanMembers)) {
                 banflag = true
+                break;
+            }
             else
                 return interaction.reply({ content: 'Jr mods do not have access to this command, please contact a mod or higher in the jr mod chat.' });
-            break;
         }
     }
     await punishUser({
@@ -119,8 +94,7 @@ export async function execute(interaction) {
         reason: reason,
         channel: interaction.channel,
         isAutomated: false,
-        currentWarnWeight: 1,
-        duration: durationMs,
+        automodWarnWeight: 1,
         banflag: banflag,
         buttonflag: false
     });
