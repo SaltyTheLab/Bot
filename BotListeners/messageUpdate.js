9aa0790412fe8a8a5bbe8e3ee19b68a7c82a67ba
Michael <michaelvargo@outlook.com>
2025-10-27 20:09:49 -0500

    remove extranious comments

--- BotListeners/messageUpdate.js
+++ BotListeners/messageUpdate.js
@@ -3,19 +3,12 @@ import guildChannelMap from "./Extravariables/guildconfiguration.json" with {typ
 export async function messageUpdate(oldMessage, newMessage) {
     const guildId = oldMessage.guild.id;
     const modChannels = guildChannelMap[guildId].modChannels
-    /**
-     * return early if no message change detected, is created by bot, or
-     * isn't in the server
-    **/
     if (!oldMessage.guild || oldMessage.author?.bot || oldMessage.content === newMessage.content) return;
 
-    //get log channel
     const logChannel = await oldMessage.guild.channels.fetch(modChannels.updatedlogChannel);
     if (!logChannel) return;
-    //created masked link of the message link
     const messageLink = `https://discord.com/channels/${oldMessage.guild.id}/${oldMessage.channel.id}/${oldMessage.id}`;
 
-    //created the embed
     const embed = new EmbedBuilder()
         .setDescription(
             `<@${newMessage.author.id}> edited a message in <#${newMessage.channelId}>\n\n` +
@@ -28,6 +21,5 @@ export async function messageUpdate(oldMessage, newMessage) {
         .setFooter({ text: `ID: ${newMessage.id}` })
         .setTimestamp();
 
-    // send the embed
     await logChannel.send({ embeds: [embed] });
 }
