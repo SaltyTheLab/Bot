d8e787c212827fb939ce4c6bb13db7f79fefda1e
Michael <michaelvargo@outlook.com>
2025-10-31 21:28:16 -0500

    move all let variable setting into the switch statement

--- moderation/punishUser.js
+++ moderation/punishUser.js
@@ -1,5 +1,5 @@
 import { EmbedBuilder, GuildMember } from 'discord.js';
-import { addPunishment, getActiveWarns, getUser } from '../Database/databasefunctions.js';
+import { addPunishment, getPunishments, getUser } from '../Database/databasefunctions.js';
 import logRecentCommand from '../WebsiteTool/recentcommands.js';
 import { load, save } from '../utilities/jsonloaders.js';
 import guildChannelMap from "../BotListeners/Extravariables/guildconfiguration.json" with {type: 'json'};
@@ -18,12 +18,9 @@ function getNextPunishment(weightedWarns) {
     { type: 'Mute', label: '4 hour mute', minutes: 120 },
     { type: 'Mute', label: '6 hour mute', minutes: 240 },
     { type: 'Mute', label: '6 hour mute', minutes: 360 }
-
   ];
   let index = Math.floor(weightedWarns);
-  if (index < 0) index = 0;
   if (index >= punishmentStages.length) index = punishmentStages.length - 1;
-
   const { type, label, minutes } = punishmentStages[index];;
   const unit = minutes >= 60 ? 'hour' : 'min';
   const duration = unit === 'hour' ? Math.ceil(minutes / 60) : minutes;
@@ -35,6 +32,8 @@ export default async function punishUser({ interaction = null, guild, target, mo
   const targetUser = await guild.members.fetch(target).catch(() => null) ?? await guild.client.users.fetch(target).catch(() => null);
   const userTag = targetUser instanceof GuildMember ? targetUser.user.tag : targetUser.tag
   const usercheck = await getUser(targetUser.id, guild.id);
+  const buttonmessage = messageid ? await channel.messages.fetch(messageid) : null
+  const activeWarns = await getPunishments(targetUser.id, guild.id, true)
   let bans = await load("BotListeners/Extravariables/commandsbans.json")
   let dmDescription = `<@${targetUser.id}>, you were given a \`warning\` in ${guild.name}.`;
   let logAuthor = `${moderatorUser.tag} warned a member`;
@@ -42,40 +41,14 @@ export default async function punishUser({ interaction = null, guild, target, mo
   let action = Promise.resolve();
   let logChannel = guild.channels.cache.get(guildChannelMap[guild.id].modChannels.mutelogChannel);
   let logcolor = LOG_COLORS['Warn']
-  let sentMessage = await channel.messages.fetch(messageid) ?? null;
-
-  if (!usercheck && interaction) {
-    interaction.reply('❌ User does not exist in the Database. Likely bot.'); return;
-  }
-
+  let sentMessage = null;
   let warnType = banflag ? 'Ban' : (interaction && interaction.options.getSubcommand() === 'mute') ? 'Mute' : 'Warn';
   let durationMs = 0;
   let currentWarnWeight = automodWarnWeight; // Default weight is 1 for manual warn
   let durationStr = '';
 
-  if (warnType === 'Warn') {
-    const activeWarnings = await getActiveWarns(targetUser.id, guild.id);
-    const totalWarnWeight = activeWarnings.length + currentWarnWeight;
-    const punishment = getNextPunishment(totalWarnWeight);
-    warnType = punishment.type;
-
-    if (punishment.type === 'Mute') {
-      durationMs = punishment.duration * unitMap[punishment.unit];
-      durationMs = Math.min(durationMs, MAX_TIMEOUT_MS);
-
-      durationStr = punishment.unit === 'day' ? `${Math.ceil(durationMs / unitMap.day)} day(s)`
-        : punishment.unit === 'hour' ? `${Math.ceil(durationMs / unitMap.hour)} hour(s)`
-          : `${Math.ceil(durationMs / unitMap.min)} minute(s)`;
-    }
-  }
-  if (warnType === 'Mute' && interaction) {
-    const duration = interaction.options.getInteger('duration');
-    const unit = interaction.options.getString('unit');
-    durationMs = duration * unitMap[unit];
-    durationMs = Math.min(durationMs, MAX_TIMEOUT_MS);
-    durationStr = durationMs >= unitMap.day ? `${Math.ceil(durationMs / unitMap.day)} day(s)`
-      : durationMs >= unitMap.hour ? `${Math.ceil(durationMs / unitMap.hour)} hour(s)`
-        : `${Math.ceil(durationMs / unitMap.min)} minute(s)`;
+  if (!usercheck && interaction) {
+    interaction.reply('❌ User does not exist in the Database. Likely bot.'); return;
   }
 
   switch (warnType) {
@@ -83,46 +56,63 @@ export default async function punishUser({ interaction = null, guild, target, mo
       bans.push(targetUser.id);
       await save("BotListeners/Extravariables/commandsbans.json", bans)
       commandTitle = `${userTag} was banned`;
-      dmDescription = `${targetUser}, you have been \`banned\` from ** ${guild.name} **.\n\n ** Reason **: \n\`${reason}\`\nPlease use /appeal to appeal your ban.\n\n Incase if I do not have a mutual server with you, you can use this invite link: https://discord.gg/Aszq4EDB`
+      dmDescription = `${targetUser}, you have been \`banned\` from ** ${guild.name} **.\n\n ** Reason **: \n\`${reason}\`\nPlease use /appeal to appeal your ban.\n\n Incase if I do not have a mutual server with you, you can use this invite link: https://discord.gg/qMjjyXyYbr`
       logAuthor = `${moderatorUser.tag} banned a member`
       logChannel = guild.channels.cache.get(guildChannelMap[guild.id].modChannels.banlogChannel)
       action = await guild.bans.create(targetUser.id, { reason: `Ban command: ${reason}`, deleteMessageSeconds: 604800 })
       logcolor = LOG_COLORS['Ban'];
       break;
-    case 'Mute':
+    case 'Mute': {
+      durationMs = interaction.options.getInteger('duration') * unitMap[interaction.options.getString('unit')];
+      durationMs = Math.min(durationMs, MAX_TIMEOUT_MS);
+      durationStr = durationMs >= unitMap.day ? `${Math.ceil(durationMs / unitMap.day)} day(s)`
+        : durationMs >= unitMap.hour ? `${Math.ceil(durationMs / unitMap.hour)} hour(s)`
+          : `${Math.ceil(durationMs / unitMap.min)} minute(s)`;
       commandTitle = `${userTag} was issued a ${durationStr} mute`
       dmDescription = `<@${targetUser.id}>, you were given a \`${durationStr} mute\` in ${guild.name}.`;
       logAuthor = `${moderatorUser.tag} muted a member`;
       action = targetUser.timeout(durationMs, reason)
       logcolor = LOG_COLORS['Mute'];
       break;
+    }
+    case 'Warn': {
+      const punishment = getNextPunishment(activeWarns.length + currentWarnWeight);
+      if (punishment.type === 'Mute') {
+        durationMs = Math.min(punishment.duration * unitMap[punishment.unit], MAX_TIMEOUT_MS);
+        durationStr = punishment.unit === 'day' ? `${Math.ceil(durationMs / unitMap.day)} day(s)`
+          : punishment.unit === 'hour' ? `${Math.ceil(durationMs / unitMap.hour)} hour(s)`
+            : `${Math.ceil(durationMs / unitMap.min)} minute(s)`;
+        commandTitle = `${userTag} was issued a ${durationStr} mute`
+        dmDescription = `<@${targetUser.id}>, you were given a \`${durationStr} mute\` in ${guild.name}.`;
+        logAuthor = `${moderatorUser.tag} muted a member`;
+        action = targetUser.timeout(durationMs, reason)
+        logcolor = LOG_COLORS['Mute'];
+        warnType = 'Mute'
+      }
+      break;
+    }
   }
   const commandEmbed = new EmbedBuilder()
     .setColor(logcolor)
     .setAuthor({ name: commandTitle, iconURL: targetUser.displayAvatarURL({ dynamic: true }) })
 
-
   if (!buttonflag && isAutomated)
     sentMessage = await channel.send({ embeds: [commandEmbed] });
-  else if (!buttonflag && !isAutomated && sentMessage == null)
+  else if (!isAutomated && sentMessage == null)
     sentMessage = await interaction.reply({ embeds: [commandEmbed], fetchReply: true });
 
-
-  const messageLink = sentMessage ?? `https://discord.com/channels/${guild.id}/${channel.id}/${sentMessage.id}`;
+  const messageLink = buttonmessage ?? `https://discord.com/channels/${guild.id}/${channel.id}/${sentMessage.id}`;
   const formattedReason = buttonflag ? `Button Ban: ${reason}`
     : banflag ? `Ban Command: ${reason}`
       : reason
-
   await addPunishment(targetUser.id, moderatorUser.id, reason, durationMs, warnType, currentWarnWeight, channel.id, guild.id, messageLink).catch(err => console.warn(err));
 
-  const activeWarnings = await getActiveWarns(targetUser.id, guild.id);
-
+  const activeWarnings = await getPunishments(targetUser.id, guild.id, true);
   const refrences = warnType === 'Ban' ?
     activeWarnings.filter(r => r.type === 'Ban').slice(0, 10).map((punishment, index) => { if (punishment.refrence) return `[Case ${index + 1}](${punishment.refrence})` }).filter(Boolean)
     : activeWarnings.slice(0, 10).map((punishment, index) => { if (punishment.refrence) return `[Case ${index + 1}](${punishment.refrence})` }).filter(Boolean)
 
   const { label } = getNextPunishment(activeWarnings.length);
-
   const expiresAt = new Date(Date.now() + THRESHOLD);
   const formattedExpiry = `<t:${Math.floor(expiresAt.getTime() / 1000)}:F>`;
 
@@ -140,10 +130,7 @@ export default async function punishUser({ interaction = null, guild, target, mo
 
   const dmEmbed = new EmbedBuilder()
     .setColor(logcolor)
-    .setAuthor({
-      name: `${userTag}`,
-      iconURL: targetUser.displayAvatarURL({ dynamic: true })
-    })
+    .setAuthor({ name: `${userTag}`, iconURL: targetUser.displayAvatarURL({ dynamic: true }) })
     .setThumbnail(guild.iconURL())
     .setDescription(dmDescription)
     .addFields(...commonFields(formattedReason, currentWarnWeight, activeWarnings.length, durationStr, formattedExpiry, warnType))
@@ -169,7 +156,7 @@ export default async function punishUser({ interaction = null, guild, target, mo
     logEmbed.setFooter({ text: 'User DMed 🚫' });
   }
   await logChannel.send({ embeds: [logEmbed] });
-  try { await action } catch (err) { interaction.reply({ content: `I couldn't ${warnType} ${userTag}: ${err}` }) }
+  await action
   if (!isAutomated)
     logRecentCommand(`${warnType} - ${userTag} - ${reason} - issuer: ${moderatorUser.tag} ${durationMs > 0 ? `- duration: ${durationStr}` : ``}`)
 }
