4d37496c012c58e61ac37cdb29d75dcf56c4f910
Michael <michaelvargo@outlook.com>
2025-11-12 11:16:11 -0600

    rename and remove recent commands

--- WebsiteTool/recentcommands.js
+++
@@ -1,9 +0,0 @@
-import { load, save } from '../utilities/fileeditors.js';
-export default async function logRecentCommand(commandString) {
-  const recentCommandsPath = `${process.cwd()}/WebsiteTool/recentCommandslog.json`;
-  const MAX_COMMANDS = 50;
-  const recentCommands = await load(recentCommandsPath);
-  recentCommands.unshift(commandString);
-  if (recentCommands.length > MAX_COMMANDS) recentCommands.length = MAX_COMMANDS;
-  await save(recentCommandsPath, recentCommands);
-}
