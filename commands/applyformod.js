6f8423cd1ce25b02caad20b3b40790a5b10e3ed2
Michael <michaelvargo@outlook.com>
2025-11-21 22:00:09 -0600

    simplify the generation of age options

--- commands/applyformod.js
+++ commands/applyformod.js
@@ -1,4 +1,4 @@
-import { SlashCommandBuilder, InteractionContextType, StringSelectMenuOptionBuilder, StringSelectMenuBuilder, ActionRowBuilder, MessageFlags, ButtonBuilder, ButtonStyle, LabelBuilder, TextInputBuilder, TextInputStyle, ModalBuilder } from "discord.js";
+import { SlashCommandBuilder, InteractionContextType, ActionRowBuilder, MessageFlags, ButtonBuilder, ButtonStyle, LabelBuilder, TextInputBuilder, TextInputStyle, ModalBuilder, StringSelectMenuBuilder } from "discord.js";
 import { save, load } from "../utilities/fileeditors.js";
 const filepath = "Extravariables/applications.json"
 export const data = new SlashCommandBuilder()
@@ -9,23 +9,12 @@ export const data = new SlashCommandBuilder()
 export async function execute(interaction) {
     const guild = interaction.guild
     const userId = interaction.user.id
-    const ages = [
-        { label: '12 or under', range: '12 or under' },
-        { label: '13 to 15', range: '13-15' },
-        { label: '16 to 17', range: '16-17' },
-        { label: '18 or over', range: '18 or over' }
-    ]
-    let applications = await load(filepath)
-    const application = applications[interaction.user.id]
-    if (application && application.Activity) {
+    const applications = load(filepath)
+    if (applications[interaction.user.id] && applications[interaction.user.id].Activity) {
         interaction.reply({
             content: 'You have already filled out the first part. Click the button below to continue to the next section.',
             components: [new ActionRowBuilder({
-                components: new ButtonBuilder({
-                    custom_id: 'next_modal_two',
-                    label: 'skip Part 1',
-                    style: ButtonStyle.Primary
-                })
+                components: [new ButtonBuilder({ custom_id: 'next_modal_two', label: 'skip Part 1', style: ButtonStyle.Primary })]
             })],
             flags: MessageFlags.Ephemeral
         })
@@ -33,58 +22,34 @@ export async function execute(interaction) {
     } else {
         applications[userId] = {}
         applications[userId].Agerange = null
-        save(filepath, applications)
-        await save(filepath, applications);
+        save(filepath, applications);
         const questionOne = new LabelBuilder({
             label: `What age range are you in?`,
             component: new StringSelectMenuBuilder({
-                max_values: 1,
-                custom_id: 'age',
-                options: ages.map(age => new StringSelectMenuOptionBuilder({
-                    label: age.label,
-                    value: age.range
-                }))
+                max_values: 1, require: true, custom_id: 'age',
+                options: [
+                    { label: '12 or under', value: '12 or under' },
+                    { label: '13 to 15', value: '13-15' },
+                    { label: '16 to 17', value: '16-17' },
+                    { label: '18 or over', value: '18 or over' }
+                ]
             })
         })
         const questionTwo = new LabelBuilder({
             label: 'Please put down any prior mod experience',
-            component: new TextInputBuilder({
-                custom_id: 'experience',
-                placeholder: 'Put your experience here or N/A',
-                required: true,
-                style: TextInputStyle.Paragraph,
-                max_length: 300
-            })
+            component: new TextInputBuilder({ custom_id: 'experience', placeholder: 'Put your experience here or N/A', required: true, style: TextInputStyle.Paragraph, max_length: 300 })
         })
         const questionThree = new LabelBuilder({
             label: 'Have you been warned/muted/kicked/banned?',
-            component: new TextInputBuilder({
-                custom_id: 'punishments',
-                placeholder: 'be honest and you do not need too much detail',
-                required: true,
-                style: TextInputStyle.Short,
-                max_length: 100
-            })
+            component: new TextInputBuilder({ custom_id: 'punishments', placeholder: 'be honest and you do not need too much detail', required: true, style: TextInputStyle.Short, max_length: 100 })
         })
         const questionFour = new LabelBuilder({
             label: 'What is your timezone?',
-            component: new TextInputBuilder({
-                custom_id: 'timezone',
-                required: true,
-                style: TextInputStyle.Short,
-                placeholder: 'If unknown, put your current time',
-                max_length: 8
-            })
+            component: new TextInputBuilder({ custom_id: 'timezone', required: true, style: TextInputStyle.Short, placeholder: 'If unknown, put your current time', max_length: 8 })
         })
-
         const questionFive = new LabelBuilder({
             label: `How active are you in ${guild.name}`,
-            component: new TextInputBuilder({
-                custom_id: 'activity',
-                style: TextInputStyle.Short,
-                required: true,
-                max_length: 150
-            })
+            component: new TextInputBuilder({ custom_id: 'activity', style: TextInputStyle.Short, required: true, max_length: 150 })
         })
         interaction.showModal(new ModalBuilder({
             title: 'Experience and Activity (1/3)',
