23c02ab064ffb85f1aeb5d105700bc7410ef4685
Michael <michaelvargo@outlook.com>
2025-11-25 21:50:53 -0600

    move unwarn logic outside the database function

--- commands/unwarn.js
+++ commands/unwarn.js
@@ -1,6 +1,5 @@
 import { SlashCommandBuilder, EmbedBuilder, InteractionContextType, PermissionFlagsBits } from "discord.js";
-import { getPunishments } from "../Database/databasefunctions.js";
-
+import { getPunishments, editPunishment } from '../Database/databaseAndFunctions.js';
 export const data = new SlashCommandBuilder()
     .setName('unwarn')
     .setDescription('removes a users most recent warn')
@@ -9,16 +8,13 @@ export const data = new SlashCommandBuilder()
     .addUserOption(opt =>
         opt.setName('target').setDescription('User to remove warn from').setRequired(true)
     )
-
 export async function execute(interaction) {
     const user = interaction.options.getUser('target')
-    const outcome = await getPunishments(user.id, interaction.guild.id, true, true)
-    const commandembed = new EmbedBuilder({ description: `recent warn removed from${user}`, color: 0x007800 })
-    outcome == 0 ? commandembed
-        .setDescription(`No warns removed from${user}`)
-        .setColor(0x700202)
-        : null
-    interaction.reply({
-        embeds: [commandembed]
-    })
+    let punishments = await getPunishments(user.id, interaction.guild.id, true)
+    if (punishments.length < 1)
+        return interaction.reply({ embeds: [new EmbedBuilder({ description: `no warns found for ${user}` })] })
+    const warns = punishments.filter(warn => warn.type == 'Warn')
+    const recentwarn = warns[punishments.length - 1]
+    editPunishment({ userId: user.id, guildId: interaction.guild.id, id: recentwarn._id })
+    return interaction.reply({ embeds: [new EmbedBuilder({ description: `recent warn removed from ${user}` })] })
 }
