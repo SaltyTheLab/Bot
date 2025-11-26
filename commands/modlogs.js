3159e2b6718073018a7f6f79125267613696a300
Michael <michaelvargo@outlook.com>
2025-11-25 21:54:01 -0600

    clean up buildLogEmbed logic and remove button building function

--- commands/modlogs.js
+++ commands/modlogs.js
@@ -1,39 +1,18 @@
-import { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits, InteractionContextType, ActionRowBuilder, ButtonStyle, ButtonBuilder, MessageFlags } from 'discord.js';
-import { editPunishment, getPunishments, getUser } from '../Database/databasefunctions.js';
-async function buildLogEmbed(interaction, log, idx, totalLogs) {
+import { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits, InteractionContextType, ActionRowBuilder, ButtonStyle, ButtonBuilder } from 'discord.js';
+import { editPunishment, getPunishments, getUser } from '../Database/databaseAndFunctions.js';
+async function buildLogEmbed(interaction, targetUser, log, idx, totalLogs) {
     const LOG_COLORS = { Warn: 0xffcc00, Mute: 0xff4444, Ban: 0xd10000, Kick: 0x838383 };
-    const [targetUser, moderator] = await Promise.all(
-        [interaction.client.users.fetch(log.userId),
-        interaction.client.users.fetch(log.moderatorId)
-        ]);
-    const formattedDate = new Date(log.timestamp).toLocaleString('en-US', {
-        dateStyle: 'medium',
-        timeStyle: 'short',
-        timeZone: 'CST'
-    });
-
-    const fields = [
-        { name: 'Member', value: `<@${log.userId}>`, inline: true },
-        { name: 'Type', value: `\`${log.type}\``, inline: true },
-    ];
-    //add mute duration if log type is mute
-    if (log.type === 'Mute' && log.duration) {
-        const totalMinutes = Math.round(log.duration / 60000); // convert ms to minutes
-        const hours = Math.floor(totalMinutes / 60);
-
-        let durationString;
-        if (hours > 0) {
-            durationString = `${hours} hour${hours > 1 ? 's' : ''}`;
-        } else {
-            durationString = `${totalMinutes} minute${totalMinutes !== 1 ? 's' : ''}`;
-        }
-        fields.push({ name: 'Duration', value: `\`${durationString}\``, inline: false });
-    }
+    const moderator = await interaction.client.users.fetch(log.moderatorId);
+    const formattedDate = new Date(log.timestamp).toLocaleString('en-US', { dateStyle: 'medium', timeStyle: 'short', timeZone: 'CST' });
+    const mins = Math.round(log.duration / 60000);
+    const hours = Math.floor(mins / 60);
     return new EmbedBuilder({
         color: LOG_COLORS[log.type],
         thumbnail: { url: targetUser.displayAvatarURL({ dynamic: true }) },
         fields: [
-            ...fields,
+            { name: 'Member', value: `<@${log.userId}>`, inline: true },
+            { name: 'Type', value: `\`${log.type}\``, inline: true },
+            ...log.duration ? { name: 'Duration', value: `\`${hours > 0 ? `${hours} hour${hours > 1 ? 's' : ''}` : `${mins} minute${mins !== 1 ? 's' : ''}`}\``, inline: false } : [],
             { name: 'Reason', value: `\`${log.reason}\``, inline: false },
             { name: 'Warns at Log Time', value: `\`${log.weight}\``, inline: true },
             { name: 'Log Status', value: log.active == 1 ? '✅ Active' : '❌ Inactive/cleared', inline: true },
@@ -41,34 +20,6 @@ async function buildLogEmbed(interaction, log, idx, totalLogs) {
         ],
         footer: { text: `Staff: ${moderator.tag} | Log ${idx + 1} of ${totalLogs} | ${formattedDate}`, iconURL: moderator.displayAvatarURL({ dynamic: true }) }
     })
-
-};
-function buildButtons(idx, totalLogs, isDeletable, logId, disabled = false) {
-    const buttons = [
-        new ButtonBuilder({
-            custom_id: `modlog-prev`,
-            label: '⬅️ Back',
-            style: ButtonStyle.Secondary,
-            disabled: idx === 0 || disabled
-        }),
-        new ButtonBuilder({
-            custom_id: `modlog-next`,
-            label: 'Next ➡️',
-            style: ButtonStyle.Secondary,
-            disabled: idx >= totalLogs - 1 || disabled
-        })
-    ];
-    if (isDeletable) {
-        buttons.push(
-            new ButtonBuilder({
-                custom_id: `modlog-del-${logId}`,
-                label: 'Delete',
-                style: ButtonStyle.Danger,
-                disabled: disabled
-            })
-        );
-    }
-    return new ActionRowBuilder({ components: buttons })
 };
 export const data = new SlashCommandBuilder()
     .setName('modlogs')
@@ -78,7 +29,6 @@ export const data = new SlashCommandBuilder()
     .addUserOption(option =>
         option.setName('user').setDescription('The user to view').setRequired(true)
     );
-
 export async function execute(interaction) {
     const targetUser = interaction.options.getUser('user');
     const fiveMinutesInMs = 5 * 60 * 1000;
@@ -87,75 +37,76 @@ export async function execute(interaction) {
     let allLogs = await getPunishments(targetUser.id, interaction.guild.id);
     if (!usercheck) {
         return interaction.reply({
-            embeds: [new EmbedBuilder({
-                color: 0x8d0b0b,
-                author: { name: `❌ ${targetUser.tag} does not exist in Database!` }
-            })],
+            embeds: [new EmbedBuilder({ color: 0x8d0b0b, author: { name: `❌ ${targetUser.tag} does not exist in Database!` } })]
         });
     } else if (!allLogs.length) {
         return interaction.reply({
-            embeds: [new EmbedBuilder({
-                color: 0xf58931,
-                author: { name: `⚠️ No modlogs found for ${targetUser.tag}.` }
-            })],
+            embeds: [new EmbedBuilder({ color: 0xf58931, author: { name: `⚠️ No modlogs found for ${targetUser.tag}.` } })],
         });
     }
-
     let currentIndex = 0;
     let currentLog = allLogs[currentIndex];
-    let replyMessage = await interaction.reply({
-        embeds: [await buildLogEmbed(interaction, currentLog, currentIndex, allLogs.length)],
-        components: [buildButtons(currentIndex, allLogs.length, isAdmin, currentLog._id)],
+    const initialResponse = await interaction.reply({
+        embeds: [await buildLogEmbed(interaction, targetUser, currentLog, currentIndex, allLogs.length)],
+        components: [new ActionRowBuilder({
+            components:
+                [new ButtonBuilder({ custom_id: `prev`, label: '⬅️ Back', style: ButtonStyle.Secondary, disabled: currentIndex === 0 }),
+                new ButtonBuilder({ custom_id: `next`, label: 'Next ➡️', style: ButtonStyle.Secondary, disabled: currentIndex >= allLogs.length - 1 }),
+                isAdmin ? new ButtonBuilder({ custom_id: `del`, label: 'Delete', style: ButtonStyle.Danger, disabled: false }) : []]
+        })],
+        withResponse: true
     });
-
-    const collector = replyMessage.createMessageComponentCollector({
+    const collector = initialResponse.resource.message.createMessageComponentCollector({
         filter: i => i.user.id === interaction.user.id,
         time: fiveMinutesInMs
     });
-
+    let replyMessage = initialResponse.resource.message;
     collector.on('collect', async i => {
-        const customIdParts = i.customId.split('-')
-        const action = customIdParts[1];
-        const logIdToDelete = customIdParts[2];
         await i.deferUpdate();
-        switch (action) {
+        switch (i.customId) {
             case 'del':
-                try {
-                    editPunishment({ userId: targetUser.id, guildId: interaction.guild.id, id: logIdToDelete })
-                    allLogs = await getPunishments(targetUser.id, interaction.guild.id);
-                    if (allLogs.length === 0) {
-                        replyMessage = await i.editReply({
-                            embeds: [new EmbedBuilder({
-                                description: `All modlogs for ${targetUser} have been deleted.`
-                            })],
-                            components: []
-                        });
-                        collector.stop();
-                        return;
-                    }
-                    currentIndex = Math.min(currentIndex, allLogs.length - 1)
-                } catch (error) {
-                    console.error(`Error deleting log ${logIdToDelete}:`, error);
-                    await i.followUp({ content: `Failed to delete log: ${error.message}`, flags: MessageFlags.Ephemeral });
+                await editPunishment({ userId: targetUser.id, guildId: interaction.guild.id, id: currentLog._id })
+                allLogs = await getPunishments(targetUser.id, interaction.guild.id);
+                if (allLogs.length < 1) {
+                    await replyMessage.edit({
+                        embeds: [new EmbedBuilder({ description: `All modlogs for ${targetUser} have been deleted.` })],
+                        components: []
+                    });
+                    return;
                 }
+                currentIndex = Math.min(currentIndex, allLogs.length - 1)
                 break;
-            default: currentIndex = action == 'next' ? Math.min(allLogs.length - 1, currentIndex + 1)
+            default: currentIndex = i.customId == 'next' ? Math.min(allLogs.length - 1, currentIndex + 1)
                 : Math.max(0, currentIndex - 1)
                 break;
         }
         currentLog = allLogs[currentIndex];
-        replyMessage = i.editReply({
-            embeds: [await buildLogEmbed(interaction, currentLog, currentIndex, allLogs.length)],
-            components: [buildButtons(currentIndex, allLogs.length, isAdmin, currentLog._id)],
+        replyMessage = await replyMessage.edit({
+            embeds: [await buildLogEmbed(interaction, targetUser, currentLog, currentIndex, allLogs.length)],
+            components: [new ActionRowBuilder({
+                components: [
+                    new ButtonBuilder({ custom_id: `prev`, label: '⬅️ Back', style: ButtonStyle.Secondary, disabled: currentIndex === 0 }),
+                    new ButtonBuilder({ custom_id: `next`, label: 'Next ➡️', style: ButtonStyle.Secondary, disabled: currentIndex >= allLogs.length - 1 }),
+                    ...(isAdmin ? new ButtonBuilder({ custom_id: `del`, label: 'Delete', style: ButtonStyle.Danger, disabled: false }) : [])
+                ]
+            })],
         });
     });
     collector.on('end', async () => {
-        try {
-            if (replyMessage.embeds.length > 0 && replyMessage.components[0]) {
-                replyMessage.edit({ components: [buildButtons(currentIndex, allLogs.length, isAdmin, currentLog._id, true)] });
+        if (allLogs.length > 0) {
+            try {
+                replyMessage.edit({
+                    components: [new ActionRowBuilder({
+                        components: [
+                            new ButtonBuilder({ custom_id: `prev`, label: '⬅️ Back', style: ButtonStyle.Secondary, disabled: true }),
+                            new ButtonBuilder({ custom_id: `next`, label: 'Next ➡️', style: ButtonStyle.Secondary, disabled: true }),
+                            ...(isAdmin ? new ButtonBuilder({ custom_id: `del`, label: 'Delete', style: ButtonStyle.Danger, disabled: true }) : [])
+                        ]
+                    })]
+                });
+            } catch (error) {
+                console.error('Error editing message on collector end:', error);
             }
-        } catch (error) {
-            console.error('Failed to disable buttons automatically:', error);
         }
     });
 }
