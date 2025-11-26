2b915ab05b3063da105824b532a9234c192cbe59
Michael <michaelvargo@outlook.com>
2025-11-25 21:52:46 -0600

    remove button building function and build them in directly

--- commands/note.js
+++ commands/note.js
@@ -1,6 +1,19 @@
 import { EmbedBuilder, SlashCommandBuilder, PermissionFlagsBits, InteractionContextType, ActionRowBuilder, ButtonBuilder, ButtonStyle, MessageFlags } from "discord.js";
-import { viewNotes, getUser, editNote } from "../Database/databasefunctions.js";
-
+import { viewNotes, getUser, editNote } from '../Database/databaseAndFunctions.js';
+async function buildNoteEmbed(interaction, targetUser, index, currentNote, length) {
+    const mod = await interaction.client.users.fetch(currentNote.moderatorId);
+    const formattedDate = new Date(currentNote.timestamp).toLocaleString('en-US', {
+        dateStyle: 'medium',
+        timeStyle: 'short',
+        timeZone: 'CST'
+    });
+    return new EmbedBuilder({
+        color: 0xdddddd,
+        thumbnail: { url: targetUser.displayAvatarURL({ dynamic: true }) },
+        description: `<@${currentNote.userId}> notes |  \`${index + 1} of ${length}\`\n> ${currentNote.note}`,
+        footer: { text: `${mod.tag} | ${formattedDate}`, iconURL: mod.displayAvatarURL({ dynamic: true }) }
+    })
+};
 export const data = new SlashCommandBuilder()
     .setName('note')
     .setDescription('add/show a users notes')
@@ -15,151 +28,108 @@ export const data = new SlashCommandBuilder()
     .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers)
     .setContexts([InteractionContextType.Guild])
 
-async function buildNoteEmbed(interaction, index, currentNote, length) {
-    const [target, mod] = await Promise.all(
-        [interaction.client.users.fetch(currentNote.userId),
-        interaction.client.users.fetch(currentNote.moderatorId)
-        ]);
-    const formattedDate = new Date(currentNote.timestamp).toLocaleString('en-US', {
-        dateStyle: 'medium',
-        timeStyle: 'short',
-        timeZone: 'CST'
-    });
-
-    return new EmbedBuilder({
-        color: 0xdddddd,
-        thumbnail: { url: target.displayAvatarURL({ dynamic: true }) },
-        description: [
-            `${target} notes |  \`${index + 1} of ${length}\``,
-            `> ${currentNote.note}`
-        ].join('\n'),
-        footer: { text: `${mod.tag} | ${formattedDate}`, iconURL: mod.displayAvatarURL({ dynamic: true }) }
-    })
-};
-function buildNoteButtons(index, allnotes, id, disabled = false) {
-    return new ActionRowBuilder({
-        components: [new ButtonBuilder({
-            custom_id: `note-prev`,
-            label: '◀️ prev',
-            style: ButtonStyle.Secondary,
-            disabled: index === 0 || disabled
-        }),
-        new ButtonBuilder({
-            custom_id: `note-next`,
-            label: '▶️ next',
-            style: ButtonStyle.Secondary,
-            disabled: index >= allnotes.length - 1 || disabled
-        }),
-        new ButtonBuilder({
-            custom_id: `note-del-${id}`,
-            label: '🗑️ delete',
-            style: ButtonStyle.Danger,
-            disabled: disabled
-        })]
-    })
-};
 export async function execute(interaction) {
-    const command = interaction.options.getSubcommand();
     const targetUser = interaction.options.getUser('target')
     const note = interaction.options.getString('note')
     const isAdmin = interaction.member.permissions.has(PermissionFlagsBits.Administrator)
     const guildId = interaction.guild.id
-    const notecommand = {
-        'add': async () => {
+    const fiveMinutesInMs = 5 * 60 * 1000;
+    if (!await getUser(targetUser.id, interaction.guild.id, true))
+        return interaction.reply({ content: `❌ ${targetUser.tag} does not exist in the User Database.` })
+    switch (interaction.options.getSubcommand()) {
+        case 'add':
             try {
                 editNote({ userId: targetUser.id, moderatorId: interaction.user.id, note: note, guildId: guildId })
             } catch (err) {
                 console.warn(`is not in the user database.`, err)
-                interaction.reply({ content: `${targetUser.tag} is not in the User Database` })
+                return interaction.reply({ content: `${targetUser.tag} is not in the User Database` });
             }
-            const commandembed = new EmbedBuilder({
-                color: 0x00a900,
-                description: [
-                    `📝 note created for <@${targetUser.id}>\n`,
-                    ` > ${note}`
-                ].join('\n\n')
-            })
             interaction.reply({
-                embeds: [commandembed]
+                embeds: [new EmbedBuilder({
+                    color: 0x00a900,
+                    description: [
+                        `📝 note created for <@${targetUser.id}>\n`,
+                        ` > ${note}`
+                    ].join('\n\n')
+                })]
             })
-        },
-        'show': async () => {
-            const fiveMinutesInMs = 5 * 60 * 1000;
-            const usercheck = getUser(targetUser.id, interaction.guild.id, true)
+            break;
+        case 'show': {
             let allnotes = await viewNotes(targetUser.id, interaction.guild.id);
-            if (!usercheck)
-                return interaction.reply({ content: `❌ ${targetUser.tag} does not exist in the User Database.` })
-            else if (!allnotes.length) {
+            if (!allnotes.length) {
                 return interaction.reply({
-                    embeds: [
-                        new EmbedBuilder({
-                            color: 0xa9a900,
-                            description: 'No notes found for that user'
-                        })]
+                    embeds: [new EmbedBuilder({ color: 0xa9a900, description: 'No notes found for that user' })]
                 });
             }
             let currentIndex = 0;
             let currentnote = allnotes[currentIndex]
-            let replyMessage = await interaction.reply({
-                embeds: [await buildNoteEmbed(interaction, currentIndex, currentnote, allnotes.length)],
-                components: [buildNoteButtons(currentIndex, allnotes, currentnote._id)]
+            const initialResponse = await interaction.reply({
+                embeds: [await buildNoteEmbed(interaction, targetUser, currentIndex, currentnote, allnotes.length)],
+                components: [new ActionRowBuilder({
+                    components: [
+                        new ButtonBuilder({ custom_id: `prev`, label: '◀️ prev', style: ButtonStyle.Secondary, disabled: currentIndex === 0 }),
+                        new ButtonBuilder({ custom_id: `next`, label: '▶️ next', style: ButtonStyle.Secondary, disabled: currentIndex >= allnotes.length - 1 }),
+                        new ButtonBuilder({ custom_id: `del`, label: '🗑️ delete', style: ButtonStyle.Danger, disabled: false })
+                    ]
+                })],
+                withResponse: true
             });
-            const collector = replyMessage.createMessageComponentCollector({
+            const collector = initialResponse.resource.message.createMessageComponentCollector({
                 filter: i => i.user.id === interaction.user.id,
                 time: fiveMinutesInMs
             })
+            let replyMessage = initialResponse.resource.message;
             collector.on('collect', async i => {
-                const customIdParts = i.customId.split('-');
-                const action = customIdParts[1];
-                const noteIdToDelete = customIdParts[2];
                 await i.deferUpdate();
-                switch (action) {
-                    case 'del':
-                        try {
-                            const twoDays = 48 * 60 * 60 * 1000
-                            if (currentnote.timestamp - Date.now() < twoDays || isAdmin) {
-                                editNote({ userId: targetUser.id, guildId: guildId, id: noteIdToDelete })
-                                allnotes = await viewNotes(targetUser.id, interaction.guild.id);
-                                if (allnotes.length === 0) {
-                                    replyMessage = await replyMessage.edit({
-                                        embeds: [new EmbedBuilder({
-                                            description: `All notes for ${targetUser} have been deleted`
-                                        })],
-                                        components: []
-                                    });
-                                    collector.stop();
-                                    return;
-                                }
-                            } else
-                                interaction.reply({
-                                    content: `${interaction.user}, please contact an admin to delete this note as two days have passed.`,
-                                    flags: MessageFlags.Ephemeral
-                                })
-                            currentIndex = Math.min(currentIndex, allnotes.length - 1)
-                        } catch (error) {
-                            console.error(`Error deleting log ${noteIdToDelete}:`, error);
-                            i.followUp({ content: `Failed to delete note: ${error.message}`, ephemeral: true });
-                        }
+                switch (i.customId) {
+                    case 'del': {
+                        if (currentnote.timestamp - Date.now() < 48 * 60 * 60 * 1000 || isAdmin) {
+                            editNote({ userId: targetUser.id, guildId: guildId, id: currentnote._id })
+                            allnotes = await viewNotes(targetUser.id, interaction.guild.id);
+                            if (allnotes.length === 0) {
+                                replyMessage.edit({
+                                    embeds: [new EmbedBuilder({ description: `All notes for ${targetUser} have been deleted` })],
+                                    components: []
+                                });
+                                return;
+                            }
+                        } else
+                            interaction.reply({
+                                content: `${interaction.user}, the time for mods to delete this note has expired, please contact an admin.`,
+                                flags: MessageFlags.Ephemeral
+                            })
+                        currentIndex = Math.min(currentIndex, allnotes.length - 1)
                         break;
-                    default: currentIndex = action == 'next' ? Math.min(allnotes.length - 1, currentIndex + 1)
+                    }
+                    default: currentIndex = i.customId == 'next' ? Math.min(allnotes.length - 1, currentIndex + 1)
                         : Math.max(0, currentIndex - 1)
                         break;
+
                 }
                 currentnote = allnotes[currentIndex]
-                replyMessage = i.editReply({
-                    embeds: [await buildNoteEmbed(interaction, currentIndex, currentnote, allnotes.length)],
-                    components: [buildNoteButtons(currentIndex, allnotes, currentnote._id,)]
+                replyMessage = await replyMessage.edit({
+                    embeds: [await buildNoteEmbed(interaction, targetUser, currentIndex, currentnote, allnotes.length)],
+                    components: [new ActionRowBuilder({
+                        components: [
+                            new ButtonBuilder({ custom_id: `prev`, label: '◀️ prev', style: ButtonStyle.Secondary, disabled: currentIndex === 0 }),
+                            new ButtonBuilder({ custom_id: `next`, label: '▶️ next', style: ButtonStyle.Secondary, disabled: currentIndex >= allnotes.length - 1 }),
+                            new ButtonBuilder({ custom_id: `del`, label: '🗑️ delete', style: ButtonStyle.Danger, disabled: false })
+                        ]
+                    })]
                 });
             });
             collector.on('end', async () => {
-                try {
-                    if (replyMessage.embeds > 0 && replyMessage.components[0])
-                        replyMessage.edit({ components: [buildNoteButtons(currentIndex, allnotes, currentnote._id, true)] });
-                } catch (error) {
-                    console.error('Failed to disable buttons automatically:', error);
-                }
+                if (allnotes.length > 0) replyMessage.edit({
+                    components: [new ActionRowBuilder({
+                        components: [
+                            new ButtonBuilder({ custom_id: `prev`, label: '◀️ prev', style: ButtonStyle.Secondary, disabled: true }),
+                            new ButtonBuilder({ custom_id: `next`, label: '▶️ next', style: ButtonStyle.Secondary, disabled: true }),
+                            new ButtonBuilder({ custom_id: `del`, label: '🗑️ delete', style: ButtonStyle.Danger, disabled: true })
+                        ]
+                    })]
+                });
             });
         }
+            break;
     }
-    notecommand[command]()
 }
