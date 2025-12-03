d7da28c7792a69beda6a497a861e43645eee72c3
Michael <michaelvargo@outlook.com>
2025-12-02 23:48:21 -0600

    move finfiles to index

--- utilities/fileeditors.js
+++ utilities/fileeditors.js
@@ -1,5 +1,4 @@
-import { readFile, writeFile, readdir } from 'fs/promises';
-import { join } from 'node:path';
+import { readFile, writeFile } from 'fs/promises';
 
 export async function load(filepath) {
     return await JSON.parse(await readFile(filepath, 'utf8'));;
@@ -7,15 +6,4 @@ export async function load(filepath) {
 
 export async function save(filepath, data) {
     await writeFile(filepath, JSON.stringify(data, null, 2))
-}
-
-export async function findFiles(dir) {
-    const filePaths = [];
-    try {
-        for (const dirent of await readdir(dir, { withFileTypes: true }))
-            if (dirent.isFile() && dirent.name.endsWith('.js'))
-                filePaths.push(join(dir, dirent.name));
-            else continue;
-    } catch {/* empty */ }
-    return filePaths;
 }
