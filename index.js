44a8fdd2844f2f53b81fc71aae4641cff5d6dd18
Michael <michaelvargo@outlook.com>
2025-12-02 23:44:57 -0600

    move deploy commands into index

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
-import { Client, GatewayIntentBits, Collection, Options, ActivityType } from 'discord.js';
+import { Client, GatewayIntentBits, Collection, Options, ActivityType, Sweepers, Routes } from 'discord.js';
 import { config } from 'dotenv';
 import { pathToFileURL } from 'node:url';
-import { save, findFiles } from './utilities/fileeditors.js';
+import { save } from './utilities/fileeditors.js';
+import { join } from 'node:path';
+import { readdir } from 'node:fs/promises';
 import { initializeRankCardBase } from './utilities/rankcardgenerator.js';
-import loadCommandsToClient from './deploy-cmds.js';
 import CountingStateManager from './Extravariables/counting.js';
 import guildChannelMap from "./Extravariables/guildconfiguration.json" with {type: 'json'}
 import embedsenders from './embeds/embeds.js';
 import db from './Database/databaseAndFunctions.js';
-import embedIDs from './embeds/EmbedIDs.json' with {type: 'json'}
-const embeds = Object.values(embedIDs).flatMap(guildEmbeds => guildEmbeds.map(embedInfo => embedInfo.messageId))
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildModeration, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildModeration, GatewayIntentBits.GuildInvites, GatewayIntentBits.MessageContent],
   partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'GUILD_MEMBER', 'USER'],
   sweepers: {
     ...Options.DefaultSweeperSettings,
-    messages: { interval: 1800, lifetime: 3600, filter: (message) => embeds.includes(message.id) },
-    users: { interval: 1800, lifetime: 3600, filter: (user) => user.id !== client.user.id }
-  }
+    messages: { interval: 120, filter: Sweepers.outdatedMessageSweepFilter(240) },
+    invites: { interval: 30, filter: Sweepers.expiredInviteSweepFilter(60) }
+  },
+  rest: { version: 10, hashSweepInterval: 2 * 60 * 60 * 1000, hashLifetime: 6 * 60 * 60 * 1000 },
+  ws: { version: 10, large_threshold: 200 },
+  waitGuildTimeout: 10 * 1000,
+  shardCount: 1
 });
-client.commands = new Collection()
 client.countingState = CountingStateManager
+client.commands = new Collection();
 config();
 const starttime = Date.now()
 async function clearExpiredWarns(usersCollection) {
@@ -41,23 +44,58 @@ function updateStatus() {
   seconds %= 60;
   client.user.setActivity(`${days}d ${hours}h ${minutes}m ${seconds}s`, { type: ActivityType.Watching });
 }
+async function getCommandData(filePaths) {
+  const fullmodule = [];
+  const jsonPayloads = [];
+  for (const filePath of filePaths) {
+    const command = await import(pathToFileURL(filePath).href);
+    if (command.data && typeof command.execute === 'function') {
+      fullmodule.push(command);
+      jsonPayloads.push(command.data.toJSON());
+    } else
+      console.warn(`⚠️ Skipping invalid file: ${filePath} (missing 'data' or 'execute' property).`);
+  }
+  return { fullmodule, jsonPayloads };
+}
+async function findFiles(dir) {
+  const filePaths = [];
+  try {
+    for (const dirent of await readdir(dir, { withFileTypes: true }))
+      if (dirent.isFile() && dirent.name.endsWith('.js'))
+        filePaths.push(join(dir, dirent.name));
+      else continue;
+  } catch {/* empty */ }
+  return filePaths;
+}
 async function main() {
-  const invites = {}
-  const { CLIENT_ID, TOKEN } = process.env;
+  if (!process.env.CLIENT_ID || !process.env.TOKEN) { console.error('❌ Missing required environment variables: TOKEN or CLIENT_ID.'); process.exit(1) }
   initializeRankCardBase();
-  await client.login(TOKEN);
+  await client.login(process.env.TOKEN);
   await new Promise(resolve => client.once('clientReady', resolve))
   client.removeAllListeners();
   const eventsNeedingClient = new Set(['messageCreate']);
   for (const filePath of await findFiles("BotListeners"))
     for (const [eventName, listenerFunc] of Object.entries(await import(pathToFileURL(filePath).href)))
-      client.on(eventName, eventsNeedingClient.has(eventName) ? (...args) => listenerFunc(client, ...args) : (...args) => listenerFunc(...args));
-  loadCommandsToClient(client.commands, client.guilds.cache.map(guild => guild.id), TOKEN, CLIENT_ID);
+      client.on(eventName, (...args) => eventsNeedingClient.has(eventName) ? listenerFunc(client, ...args) : listenerFunc(...args));
+  client.commands.clear();
+  const globalPaths = await findFiles('commands')
+  const globalProcessed = await getCommandData(globalPaths)
+  globalProcessed.fullmodule.forEach(command => client.commands.set(command.data.name, command))
+  client.rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: globalProcessed.jsonPayloads });
+  const guildPaths = {};
+  for (const guildId of client.guilds.cache.map(guild => guild.id))
+    guildPaths[guildId] = await findFiles(`C:/Users/micha/Desktop/Bot/commands/${guildId}`)
+  for (const guildId in guildPaths) {
+    const guildProcessed = await getCommandData(guildPaths[guildId]);
+    guildProcessed.fullmodule.forEach(command => client.commands.set(`${guildId}:${command.data.name}`, command))
+    if (guildProcessed.jsonPayloads.length > 0) client.rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, guildId), { body: guildProcessed.jsonPayloads });
+  }
   updateStatus();
   setInterval(updateStatus, 5000)
   await clearExpiredWarns(db.collection('users'))
   setInterval(async () => { await clearExpiredWarns(db.collection('users')) }, 5 * 60 * 1000)
   await embedsenders(client.channels.cache);
+  const invites = {}
   for (const [guildId, guild] of client.guilds.cache) {
     const guildinvites = await guild.invites.fetch();
     invites[guildId] = guildinvites.map(invite => { return { id: invite.code, uses: invite.uses } })
