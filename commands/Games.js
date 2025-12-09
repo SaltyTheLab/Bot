7cd31670dce622b0bf39ef15287aed54d2b99bcb
Michael <michaelvargo@outlook.com>
2025-12-09 16:53:27 -0600

    combine common commands

--- commands/Games.js
+++ commands/Games.js
@@ -1,12 +1,8 @@
-import { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonStyle, ComponentType, InteractionContextType, MessageFlags, ButtonBuilder } from "discord.js";
+import { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, InteractionContextType, ButtonBuilder } from "discord.js";
 import { getUser, saveUser } from '../Database/databaseAndFunctions.js';
 import { resolve } from 'node:path'
 import logos from "../Database/logos.json" with {type: 'json'};
 //logo game functions
-function getRandomColor() {
-    const randomHex = Math.floor(Math.random() * 16777215)
-    return parseInt(`0x${randomHex.toString(16).padStart(6, '0')}`, 16)
-}
 function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -32,37 +28,13 @@ function generateButtons(gameBoard, disabled = false) {
             row.addComponents(new ButtonBuilder({
                 custom_id: `tictactoe-${index}`,
                 label: gameBoard[index] === ' ' ? '\u200b' : gameBoard[index],
-                style: gameBoard[index] === 'X' ? ButtonStyle.Primary :
-                    gameBoard[index] === 'O' ? ButtonStyle.Danger :
-                        ButtonStyle.Secondary,
+                style: gameBoard[index] === 'X' ? 1 : gameBoard[index] === 'O' ? 4 : 2,
                 disabled: disabled || gameBoard[index] !== ' '
             }))
         }
         rows.push(row);
     }
     return rows;
-}
-
-function checkWinner(gameBoard, player) {
-    const winningConditions = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ]
-    return winningConditions.some(condition => {
-        return condition.every(index => {
-            return gameBoard[index] === player
-        });
-    });
-}
-
-function checkDraw(gameBoard) {
-    return gameBoard.every(cell => cell !== ' ')
 }
 export const data = new SlashCommandBuilder()
     .setName('games')
@@ -74,12 +46,14 @@ export const data = new SlashCommandBuilder()
                 .addUserOption(option => option.setName('opponent').setDescription('The user you want to play against').setRequired(true))))
     .addSubcommand(subcommand => subcommand.setName('rps').setDescription('Play Rock, Paper, Scissors'))
     .addSubcommand(subcommand => subcommand.setName('logos').setDescription('Play the Logo Game'))
+    .addSubcommand(subcommand => subcommand.setName('bet').setDescription('bet coins')
+        .addIntegerOption(opt => opt.setName('amount').setDescription('how many coins').setRequired(true)))
     .setContexts(InteractionContextType.Guild)
 
 export async function execute(interaction) {
     let collector;
-    const group = interaction.options.getSubcommandGroup()
-    if (group == 'tictactoe') {
+    const { userData } = await getUser({ userId: interaction.user.id, guildId: interaction.guild.id, modflag: true });
+    if (interaction.options.getSubcommandGroup() == 'tictactoe') {
         const player1 = interaction.user;
         let player2 = null;
         let isCPU = false;
@@ -96,43 +70,40 @@ export async function execute(interaction) {
                 break;
             case 'user':
                 player2 = interaction.options.getUser('opponent');;
-                if (player1.id === player2.id)
-                    return interaction.reply({ content: "You can't play against yourself.", flags: MessageFlags.Ephemeral })
+                if (player1.id === player2.id) return interaction.reply({ content: "You can't play against yourself.", flags: 64 })
                 break;
         }
         let currentplayer = Math.random() < 0.5 ? player1 : player2;
         const players = new Map();
         players.set(player1.id, 'X');
         players.set(player2.id, 'O');
-        const sentMessage = await interaction.reply({
-            embeds: [generateEmbed(`It's ${currentplayer}'s turn to move`)], components: generateButtons(gameBoard), withResponse: true
-        });
-        collector = interaction.channel.createMessageComponentCollector({
-            ComponentType: ComponentType.Button,
+        const sentMessage = await interaction.reply({ embeds: [generateEmbed(`It's ${currentplayer}'s turn to move`)], components: generateButtons(gameBoard), withResponse: true });
+        collector = sentMessage.resource.message.createMessageComponentCollector({
+            ComponentType: 2,
             filter: i => i.message.id === sentMessage.interaction.responseMessageId,
             time: 120000
         })
         async function handleMoveAndStateUpdate(moveIndex, playerMark, updateCallback) {
             gameBoard[moveIndex] = playerMark;
-            if (checkWinner(gameBoard, playerMark)) {
-                collector.stop('win');
+            let win, draw = null
+            const winningConditions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+            if (winningConditions.some(condition => { return condition.every(index => { return gameBoard[index] === playerMark }); })) {
+                updateCallback({ embeds: [generateEmbed(`${currentplayer} wins!!`, 0xffd700)], components: generateButtons(gameBoard, true) });
                 if (!isCPU) {
-                    const { userData } = await getUser(currentplayer.id, interaction.guild.id, true)
+                    const { userData } = await getUser({ userId: currentplayer.id, guildId: interaction.guild.id, modflag: true })
                     userData.coins += 100;
                     saveUser({ userId: currentplayer.id, guildId: interaction.guild.id, userData: userData })
                 }
+                win = true;
                 return true;
             }
-            if (checkDraw(gameBoard)) {
-                updateCallback({ components: generateButtons(gameBoard, true) });
-                collector.stop('draw');
+            if (gameBoard.every(cell => cell !== ' ')) {
+                updateCallback({ embeds: [generateEmbed(`It's a draw!`, 0x555555)], components: generateButtons(gameBoard, true) });
+                draw = true;
                 return true;
             }
-            currentplayer = (currentplayer.id === player1.id) ? player2 : player1;
-            await updateCallback({
-                embeds: [generateEmbed(`It's ${currentplayer.toString()}'s turn to move.`)],
-                components: generateButtons(gameBoard)
-            });
+            if (!win && !draw) currentplayer = (currentplayer.id === player1.id) ? player2 : player1;
+            await updateCallback({ embeds: [generateEmbed(`It's ${currentplayer.toString()}'s turn to move.`)], components: generateButtons(gameBoard) });
             return false;
         }
         if (isCPU && currentplayer.id === player2.id) {
@@ -141,50 +112,37 @@ export async function execute(interaction) {
             if (move !== -1) handleMoveAndStateUpdate(move, players.get(currentplayer.id), interaction.editReply.bind(interaction));
         }
         collector.on('collect', async i => {
-            if (i.user.id !== player1.id && i.user.id !== player2.id)
-                return i.reply({ content: 'You are not a participant in this game.', flags: MessageFlags.Ephemeral })
-            if (i.user.id !== currentplayer.id)
-                return i.reply({ content: 'It\'s not your turn!', flags: MessageFlags.Ephemeral })
+            if (i.user.id !== player1.id && i.user.id !== player2.id) return i.reply({ content: 'You are not a participant in this game.', flags: 64 })
+            if (i.user.id !== currentplayer.id) return i.reply({ content: 'It\'s not your turn!', flags: 64 })
             const move = parseInt(i.customId.split('-')[1]);
             const playerMark = players.get(currentplayer.id);
-            if (gameBoard[move] !== ' ')
-                return i.reply({ content: "That cell is already taken! Please wait for the buttons to update before making your next move.", flags: MessageFlags.Ephemeral });
+            if (gameBoard[move] !== ' ') return i.reply({ content: "That cell is already taken!", flags: 64 });
             gameBoard[move] = playerMark;
-            const gameOver = handleMoveAndStateUpdate(move, playerMark, i.update.bind(i))
+            const gameOver = await handleMoveAndStateUpdate(move, playerMark, i.update.bind(i))
             if (!gameOver && isCPU) {
                 await new Promise(r => setTimeout(r, 750));
                 const availableMoves = gameBoard.map((cell, index) => (cell === ' ' ? index : null)).filter(index => index !== null);
                 const move = availableMoves.length > 0 ? availableMoves[Math.floor(Math.random() * availableMoves.length)] : -1;
-                if (move !== -1) handleMoveAndStateUpdate(move, players.get(currentplayer.id), interaction.editReply.bind(interaction));
+                if (move !== -1) await handleMoveAndStateUpdate(move, players.get(currentplayer.id), interaction.editReply.bind(interaction));
             }
-        });
-        collector.on('end', async (collected, reason) => {
-            let finalEmbed;
-            switch (reason) {
-                case 'time': finalEmbed = generateEmbed(`The game ended due to inactivity.`, 0xff0000)
-                    break;
-                case 'win': finalEmbed = generateEmbed(`${currentplayer} wins!!`, 0xffd700)
-                    break;
-                default:
-                    finalEmbed = generateEmbed(`It's a draw!`, 0x555555)
-            }
-            interaction.editReply({ embeds: [finalEmbed], components: generateButtons(gameBoard, true) })
+            else collector.stop('end')
         });
     }
     else {
         switch (interaction.options.getSubcommand()) {
-            case 'rps':
-                interaction.reply({
+            case 'rps': {
+                const initialmessage = await interaction.reply({
                     embeds: [new EmbedBuilder({ title: '**Pick your option**', color: 0x00a900 })],
                     components: [{
                         type: 1,
                         components: [
-                            { type: 2, custom_id: 'Rock', label: 'rock', style: ButtonStyle.Secondary },
-                            { type: 2, custom_id: 'Paper', label: 'paper', style: ButtonStyle.Secondary },
-                            { type: 2, custom_id: 'Scissors', label: 'scissors', style: ButtonStyle.Secondary }]
-                    }]
+                            { type: 2, custom_id: 'Rock', label: 'rock', style: 2 },
+                            { type: 2, custom_id: 'Paper', label: 'paper', style: 2 },
+                            { type: 2, custom_id: 'Scissors', label: 'scissors', style: 2 }]
+                    }],
+                    withResponse: true
                 });
-                collector = interaction.channel.createMessageComponentCollector({
+                collector = initialmessage.resource.message.createMessageComponentCollector({
                     filter: i => i.user.id === interaction.user.id,
                     time: 15000,
                     max: 1
@@ -199,7 +157,7 @@ export async function execute(interaction) {
                         : beats[opponentchoice] === userchoice ? result = 'Febot Wins!!!'
                             : result = "it's a tie!!"
                     if (result === 'you win!!!') {
-                        const { userData } = await getUser(interaction.user.id, interaction.guild.id, true)
+                        const { userData } = await getUser({ userId: interaction.user.id, guildId: interaction.guild.id, modflag: true })
                         userData.coins += 20;
                         saveUser({ userId: interaction.user.id, guildId: interaction.guild.id, userData: userData });
                     }
@@ -213,14 +171,15 @@ export async function execute(interaction) {
                     });
                 })
                 break;
+            }
             case 'logos': {
                 const logo = logos[Math.floor(Math.random() * logos.length)];
                 const options = shuffle([logo, ...shuffle(logos.filter(l => l.brand !== logo.brand)).slice(0, 3)]);
-                let buttons = options.map(option => { return { type: 2, custom_id: option.brand, label: option.brand, style: ButtonStyle.Primary }; });
+                let buttons = options.map(option => { return { type: 2, custom_id: option.brand, label: option.brand, style: 1 }; });
                 const initialmessage = await interaction.reply({
                     embeds: [new EmbedBuilder({
                         author: { name: `Guess this logo ${interaction.user.tag}`, iconURL: interaction.user.displayAvatarURL({ dynamic: true }), },
-                        color: getRandomColor(),
+                        color: () => { const randomHex = Math.floor(Math.random() * 16777215); return parseInt(`0x${randomHex.toString(16).padStart(6, '0')}`, 16); },
                         image: { url: 'attachment://logo.png' }
                     })],
                     files: [{ attachment: resolve(`./${logo.image}`), name: 'logo.png' }],
@@ -228,18 +187,13 @@ export async function execute(interaction) {
                     withResponse: true
                 });
                 collector = initialmessage.resource.message.createMessageComponentCollector(
-                    {
-                        componentType: ComponentType.Button,
-                        filter: i => i.message.id === initialmessage.interaction.responseMessageId,
-                        time: 15000
-                    }
-                );
+                    { componentType: 2, filter: i => i.message.id === initialmessage.interaction.responseMessageId, time: 15000 });
                 const message = initialmessage.resource.message
                 collector.on('collect', async i => {
                     buttons = options.map(o => {
                         return {
                             type: 2, custom_id: o.brand, label: o.brand,
-                            style: o.brand === logo.brand ? ButtonStyle.Success : i.customId === o.brand ? ButtonStyle.Danger : ButtonStyle.Secondary,
+                            style: o.brand === logo.brand ? 3 : i.customId === o.brand ? 4 : 2,
                             disabled: true
                         }
                     });
@@ -253,10 +207,30 @@ export async function execute(interaction) {
                 });
                 collector.on('end', async (collected, reason) => {
                     if (collected.size === 0 && reason == 'time') {
-                        buttons = options.map(option => { return { type: 2, custom_id: option.brand, label: option.brand, style: ButtonStyle.Primary, disabled: true } })
+                        buttons = options.map(option => { return { type: 2, custom_id: option.brand, label: option.brand, style: 1, disabled: true } })
                         message.edit({ components: [{ type: 1, components: buttons }] })
                     }
                 })
+                break;
+            }
+            case 'bet': {
+                const coincount = interaction.options.getInteger('amount')
+                if (coincount > userData.coins) {
+                    interaction.reply({ content: `you cannot bet more than you have ${interaction.user}`, flags: 64 })
+                    return;
+                }
+                let statement = ` ${interaction.user.tag} you bet ${coincount} and won!!`
+                const result = new EmbedBuilder({ color: 0x007a00, author: { name: statement, iconURL: interaction.user.displayAvatarURL({ dyanamic: true }) }, })
+                if (Math.random() >= .5) userData.coins += Math.ceil(coincount * 1.5);
+                else {
+                    statement = ` ${interaction.user.tag} you bet ${coincount} and lost!`
+                    result.setColor(0x7a0000)
+                    result.setAuthor({ name: statement, iconURL: interaction.user.displayAvatarURL({ dyanamic: true }) })
+                    userData.coins -= coincount;
+                    userData.coins < 0 ? userData.coins = 0 : null
+                }
+                saveUser({ userId: interaction.user.tag, guildId: interaction.guild.id, userData: userData });
+                interaction.reply({ embeds: [result] })
             }
         }
     }
--- commands/ModCommands.js
+++ commands/ModCommands.js
@@ -1,9 +1,10 @@
-import { InteractionContextType, PermissionFlagsBits, SlashCommandBuilder, GuildMember, MessageFlags } from "discord.js";
+import { InteractionContextType, PermissionFlagsBits, SlashCommandBuilder, GuildMember, MessageFlags, EmbedBuilder } from "discord.js";
+import { getPunishments, editPunishment } from "../Database/databaseAndFunctions.js";
 import punishUser from "../moderation/punishUser.js";
 import guildchannelmap from "../Extravariables/guildconfiguration.json" with {type: 'json'}
 export const data = new SlashCommandBuilder()
     .setName('member')
-    .setDescription('Warn/Mute/Ban/kick a member')
+    .setDescription('Warn/Mute/Ban/kick/unwarn/unmute a member')
     .addSubcommand(command =>
         command.setName('warn').setDescription('Warn a member')
             .addUserOption(opt => opt.setName('target').setDescription('The user you want to warn').setRequired(true))
@@ -14,11 +15,7 @@ export const data = new SlashCommandBuilder()
             .addStringOption(opt => opt.setName('reason').setDescription('Reason for the mute').setRequired(true))
             .addIntegerOption(opt => opt.setName('duration').setDescription('Mute duration').setRequired(true))
             .addStringOption(opt => opt.setName('unit').setDescription('Mute unit').setRequired(true)
-                .addChoices(
-                    { name: 'Minute', value: 'min' },
-                    { name: 'Hour', value: 'hour' },
-                    { name: 'Day', value: 'day' }
-                )))
+                .addChoices({ name: 'Minute', value: 'min' }, { name: 'Hour', value: 'hour' }, { name: 'Day', value: 'day' })))
     .addSubcommand(command =>
         command.setName('ban').setDescription('Ban a user')
             .addUserOption(opt => opt.setName('target').setDescription('The user to ban').setRequired(true))
@@ -27,6 +24,12 @@ export const data = new SlashCommandBuilder()
         command.setName('kick').setDescription('Kick a user')
             .addUserOption(opt => opt.setName('target').setDescription('The user to kick').setRequired(true))
             .addStringOption(opt => opt.setName('reason').setDescription('Reason for the kick').setRequired(true)))
+    .addSubcommand(command => command.setName('unwarn').setDescription('Removes a user\'s recent warn')
+        .addUserOption(opt => opt.setName('target').setDescription('User to remove warn from').setRequired(true))
+    )
+    .addSubcommand(command => command.setName('unmute').setDescription('Unmutes a user')
+        .addUserOption(opt => opt.setName('target').setDescription('The user to unmute').setRequired(true))
+    )
     .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers)
     .setContexts([InteractionContextType.Guild])
 export async function execute(interaction) {
@@ -35,25 +38,20 @@ export async function execute(interaction) {
     const reason = interaction.options.getString('reason');
     const staffcheck = target instanceof GuildMember ? target.permissions.has(PermissionFlagsBits.ModerateMembers) : null
     const duration = interaction.options.getInteger('duration') ?? null
-    const adminChannel = await interaction.client.channels.cache.get(guildchannelmap[interaction.guild.id].modChannels.adminChannel) ?? null
+    const adminChannel = interaction.client.channels.cache.get(guildchannelmap[interaction.guild.id].modChannels.adminChannel) ?? null
     let banflag = false
     let kick = false
-    if (target.bot)
-        return interaction.reply({ content: 'You cannot ban a bot.', flags: MessageFlags.Ephemeral });
-    if (target.id === interaction.user.id)
-        return interaction.reply({ content: '⚠️ You cannot execute mod commands on yourself.', flags: MessageFlags.Ephemeral });
-    if (staffcheck)
-        return interaction.reply({ content: `${target.user.tag} is staff, please handle this with an admin, co-owner, or owner.`, flags: MessageFlags.Ephemeral })
+    if (target.bot) return interaction.reply({ content: 'You cannot ban a bot.', flags: MessageFlags.Ephemeral });
+    if (target.id === interaction.user.id) return interaction.reply({ content: '⚠️ You cannot execute mod commands on yourself.', flags: MessageFlags.Ephemeral });
+    if (staffcheck) return interaction.reply({ content: `${target.user.tag} is staff, please handle this with an admin, co-owner, or owner.`, flags: MessageFlags.Ephemeral })
     switch (command) {
         case 'mute':
-            if (duration <= 0)
-                return interaction.reply({ content: '❌ Invalid duration', flags: MessageFlags.Ephemeral });
-            if (target.communicationDisabledUntilTimestamp && target.communicationDisabledUntilTimestamp > Date.now())
-                return interaction.reply({ content: '⚠️ User is already muted.', flags: MessageFlags.Ephemeral });
+            if (duration <= 0) return interaction.reply({ content: '❌ Invalid duration', flags: MessageFlags.Ephemeral });
+            if (target.communicationDisabledUntilTimestamp && target.communicationDisabledUntilTimestamp > Date.now()) return interaction.reply({ content: '⚠️ User is already muted.', flags: MessageFlags.Ephemeral });
             break;
         case 'ban':
             if (!interaction.member.permissions.has(PermissionFlagsBits.BanMembers)) {
-                adminChannel.send({ content: `Jr. mod ${interaction.user} just tried to use the ban command, I suggest a private talk if you see multiple instances of this message about them...` })
+                adminChannel.send({ content: `Jr. mod ${interaction.user} tried to use the ban command, please talk to them if you see multiple instances of this message...` })
                 return interaction.reply({ content: 'Jr mods do not have access to this command, please contact a mod or higher in the jr mod chat.', flags: MessageFlags.Ephemeral });
             }
             banflag = true
@@ -61,15 +59,20 @@ export async function execute(interaction) {
         case 'kick':
             kick = true
             break;
+        case 'unwarn': {
+            let punishments = await getPunishments(target.id, interaction.guild.id, true)
+            if (punishments.length < 1) return interaction.reply({ embeds: [new EmbedBuilder({ description: `no warns found for ${target}` })] })
+            const warns = punishments.filter(warn => warn.type == 'Warn')
+            const recentwarn = warns[punishments.length - 1]
+            editPunishment({ userId: target.id, guildId: interaction.guild.id, id: recentwarn._id })
+            return interaction.reply({ embeds: [new EmbedBuilder({ description: `recent warn removed from ${target}` })] })
+        }
+        case 'unmute': {
+            const member = await interaction.guild.members.fetch(target.id);
+            const embed = new EmbedBuilder({ color: 0xb50000, description: `${target} is not muted.` })
+            if (member.communicationDisabledUntil) { await member.timeout(null); embed.setColor(0x00a900).setDescription(`${target.tag} was unmuted.`) }
+            return interaction.reply({ embeds: [embed] })
+        }
     }
-    punishUser({
-        interaction: interaction,
-        guild: interaction.guild,
-        target: target,
-        moderatorUser: interaction.user,
-        reason: reason,
-        channel: interaction.channel,
-        banflag: banflag,
-        kick: kick
-    });
+    punishUser({ interaction: interaction, guild: interaction.guild, target: target, moderatorUser: interaction.user, reason: reason, channel: interaction.channel, banflag: banflag, kick: kick });
 }
