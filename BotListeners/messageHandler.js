aafd475611f592d4d0291c51ba4fac3f3515bf36
Michael <michaelvargo@outlook.com>
2025-12-11 00:37:11 -0600

    rearrange logic after adding in automod function

--- BotListeners/messageHandler.js
+++ BotListeners/messageHandler.js
@@ -1,4 +1,4 @@
-import { EmbedBuilder, MessageType, MessageFlagsBitField } from "discord.js";
+import { EmbedBuilder, MessageType, MessageFlagsBitField, EmbedType } from "discord.js";
 import { getUser, saveUser } from '../Database/databaseAndFunctions.js';
 import forbbidenWordsData from '../moderation/forbiddenwords.json' with {type: 'json'};
 import globalwordsData from '../moderation/globalwords.json' with {type: 'json'}
@@ -23,7 +23,7 @@ const multireplies = [
     { keyword: ['hello', 'there'], reply: 'general Kenobi' }
 ];
 async function applyUserXP(member, channel, author, guild) {
-    const { userData, rank } = await getUser({ userId: author.id, guildId: guild.id, modflag: true });
+    const { userData, rank } = await getUser({ userId: author.id, guildId: guild.id });
     const verifiedRole = guild.roles.cache.find(r => r.name.toLowerCase() === 'verified');
     userData.xp += 20;
     userData.totalmessages += 1;
@@ -35,7 +35,7 @@ async function applyUserXP(member, channel, author, guild) {
             embeds: [new EmbedBuilder({
                 author: { name: `${author.tag} leveled up to ${userData.level}!`, iconURL: author.displayAvatarURL({ dynamic: true }) },
                 color: 0x00AE86,
-                footer: { text: `you are now rank ${rank} in ${guild.name}`, iconURL: guild.iconURL({ extension: 'png', size: 64 }) }
+                footer: { text: `you are now #${rank} in ${guild.name}`, iconURL: guild.iconURL({ extension: 'png', size: 64 }) }
             })]
         });
     }
@@ -84,71 +84,53 @@ export async function messageDelete(message) {
         }))
     logChannel.send({ embeds: imageAttachments.length > 1 ? [mainEmbed, ...additionalImageEmbeds] : [mainEmbed] });
 };
-export async function messageCreate(client, message) {
-    const { guild, author, member, content, channel, attachments, flags, type, mentions, embeds } = message;
+export async function messageCreate(message) {
+    const { guild, author, member, content, channel, attachments, flags, type, mentions, embeds, client } = message;
     if (author.bot || !guild || !member) return;
     const sentbystaff = member.permissions.has('ModerateMembers')
     // eslint-disable-next-line no-useless-escape
-    const messageWords = content.replace(/<a?:\w+:\d+>/g, '').replace(/[\-!.,?_\\*#()\[\]{}\+:;='"`~/|^&]/g, '').toLowerCase()
+    const messageWords = content.replace(/<a?:\w+:\d+>/g, '').replace(/[\-!.,?_\\*#()\[\]{}\+:;='"`~/|^&]/g, '')
     if (channel.id === guildChannelMap[guild.id].publicChannels.countingChannel) {
         const countingState = client.countingState
         if (!messageWords.trim() || isNaN(parseInt(messageWords.trim())) || Number(messageWords) !== parseInt(messageWords.trim())) return;
-        const lastUser = countingState.getLastUser(guild.id)
-        const expectedNumber = countingState.getCount(guild.id) + 1
+        const lastUser = countingState.getLastUser(guild.id); const expectedNumber = countingState.getCount(guild.id) + 1;
         if (parseInt(messageWords.trim()) === expectedNumber && lastUser !== author.id) { countingState.increaseCount(author.id, guild.id); message.react('✅'); return; }
-        else {
-            message.reply({ content: `you already counted or put a number down <@${author.id}>!(number reset)` })
-            countingState.reset(guild.id); message.react('❌'); return;
-        }
+        else message.reply({ content: `you already counted <@${author.id}>!(number reset)` }); countingState.reset(guild.id); message.react('❌'); return;
     }
-    for (const [keyword, reply] of replies) { if (messageWords.includes(keyword)) { message.reply({ content: reply }); break; } }
-    for (const { keyword, reply } of multireplies) { if (keyword.every(k => messageWords.includes(k))) { message.reply({ content: reply }); break; } }
-    if (messageWords.includes('<@857445139416088647>')) message.react('1257522749635563561');
-    if (messageWords.includes('bad') && messageWords.includes('bot')) message.react('😡');
+    const text = messageWords.toLowerCase()
+    for (const [keyword, reply] of replies) { if (text.includes(keyword)) { message.reply({ content: reply }); } }
+    for (const { keyword, reply } of multireplies) { if (keyword.every(k => text.includes(k))) { message.reply({ content: reply }); } }
+    if (text.includes('<@857445139416088647>')) message.react('1257522749635563561');
+    if (text.includes('bad') && text.includes('bot')) message.react('😡');
     if (type !== MessageType.UserJoin) applyUserXP(member, channel, author, guild)
-    if (sentbystaff) return;
-    const [exclusions, mediaexclusions] = [guildChannelMap[guild.id].exclusions, guildChannelMap[guild.id].mediaexclusions,]
-    let globalword, matchedWord = null;
-    const isChannelExcluded = Object.values(exclusions).some(id => id === channel.parentId || id === channel.id);
-    const words = messageWords.toLowerCase().split(/\s+/g);
-    for (const word of words) { if (globalWords.has(word)) { globalword = word; break; } if (!isChannelExcluded && forbiddenWords.has(word)) { matchedWord = word; break; } }
-    let capSpam;
-    if (messageWords.length >= 20) { const upperCaseOnly = messageWords.match(/[A-Z]/g); if (upperCaseOnly) capSpam = upperCaseOnly.length / messageWords.length > 0.7; }
+    if (sentbystaff || author.id === "521404063934447616") return;
     let { mediaCount, total, duplicateCounts } = userMessageTrackers.get(`${author.id}-${guild.id}`) ?? ({ total: 0, mediaCount: 0, duplicateCounts: new Map() });
-    const { Duplicatespamthreshold: duplicateThreshold, mediathreshold: mediathreshold, messagethreshold: messageThreshold } = guildChannelMap[guild.id].automodsettings;
-    total += 1;
-    const embedcheck = embeds.some(embed => {
-        const urls = [embed.image?.url, embed.video?.url, embed.thumbnail?.url].filter(Boolean);
-        return urls.some(url => /\.(gif|jpe?g|png|mp4|webm)$/i.test(url));
-    })
-    if ((attachments.size > 0 || embedcheck)
-        && !flags.has(MessageFlagsBitField.Flags.IsVoiceMessage) && !Object.values(mediaexclusions).some(id => id === channel.parentId || id === channel.id)) mediaCount += 1;
     const currentContentCount = (duplicateCounts.get(messageWords) || 0) + 1;
-    const duplicateSpam = currentContentCount > duplicateThreshold
-    const mediaViolation = mediaCount > mediathreshold && total < messageThreshold
-    if (messageWords.length > 0) duplicateCounts.set(messageWords, currentContentCount);
-    if (duplicateSpam) duplicateCounts.clear();
-    if (total >= messageThreshold) { total = 0; mediaCount = 0; }
-    else if (mediaViolation) mediaCount = 0;
-    userMessageTrackers.set(`${author.id}-${guild.id}`, { total: total, mediaCount: mediaCount, duplicateCounts: duplicateCounts })
-    const { userData } = await getUser({ userId: author.id, guildId: guild.id, modflag: true })
-    const generalspam = channel.messages.cache.filter(m => m.author.id === author.id && m.createdTimestamp > (Date.now() - 4 * 1000)).size >= 8;
-    const inviteRegex = /(https?:\/\/)?(www\.)?(discord\.gg|discord(app)?\.com\/invite)\/[a-zA-Z0-9-]+/i;
-    const [hasInvite, everyonePing] = [inviteRegex.test(messageWords), mentions.everyone];
-    if (globalword || matchedWord || hasInvite || everyonePing) await message.delete()
-    const checks = [
-        { flag: hasInvite, reason: 'Discord invite', Weight: 2 },
-        { flag: globalword, reason: "Saying a slur", Weight: 2 },
-        { flag: matchedWord, reason: `NSFW word`, Weight: 1 },
-        { flag: everyonePing, reason: 'Mass pinging', Weight: 2 },
-        { flag: generalspam, reason: 'Spamming', Weight: 1 },
-        { flag: duplicateSpam, reason: 'Spamming the same message', Weight: 1 },
-        { flag: mediaViolation, reason: 'Media violation', Weight: 1 },
-        { flag: capSpam, reason: 'Spamming Caps', Weight: 1 }
+    const [hasInvite, everyonePing, duplicateSpam, mediaViolation, generalspam] = [
+        /(https?:\/\/)?(www\.)?(discord\.gg|discord(app)?\.com\/invite)\/[a-zA-Z0-9-]+/i.test(content),
+        mentions.everyone,
+        currentContentCount > guildChannelMap[guild.id].automodsettings.Duplicatespamthreshold,
+        mediaCount > guildChannelMap[guild.id].automodsettings.mediathreshold && total < guildChannelMap[guild.id].automodsettings.messageThreshold,
+        channel.messages.cache.filter(m => m.author.id === author.id && m.createdTimestamp > (Date.now() - 4 * 1000)).size >= 8
     ];
+    const embedcheck = embeds.some(embed => { return embed.data.type == EmbedType.Image || embed.data.type == EmbedType.Video || embed.data.type == EmbedType.GIFV || embed.data.type == EmbedType.Rich })
+    if ((attachments.size > 0 || embedcheck) && !flags.has(MessageFlagsBitField.Flags.IsVoiceMessage) && !Object.values(guildChannelMap[guild.id].mediaexclusions).some(id => id === channel.parentId || id === channel.id)) mediaCount += 1;
+    total += 1;
+    userMessageTrackers.set(`${author.id}-${guild.id}`, { total: total, mediaCount: mediaCount, duplicateCounts: duplicateCounts })
+    let globalword, matchedWord, capSpam = false;
+    const words = messageWords.split(/\s+/g);
+    const isChannelExcluded = Object.values(guildChannelMap[guild.id].exclusions).some(id => id === channel.parentId || id === channel.id);
+    for (const word of words) { if (globalWords.has(word)) { globalword = word; break; } if (!isChannelExcluded && forbiddenWords.has(word)) { matchedWord = word; break; } }
+    if (messageWords) { duplicateCounts.set(messageWords, currentContentCount); if (messageWords.length >= 20) { const caps = messageWords.match(/[A-Z]/g); if (caps) capSpam = caps.length / messageWords.length > 0.7; } }
+    if (duplicateSpam) duplicateCounts.clear();
+    if (total >= guildChannelMap[guild.id].automodsettings.messageThreshold) { total = 0; mediaCount = 0; }
+    else if (mediaViolation) mediaCount = 0;
+    if (globalword || matchedWord || hasInvite || everyonePing) await message.delete()
+    const checks = [{ flag: hasInvite, reason: 'Discord invite', Weight: 2 }, { flag: globalword, reason: "Saying a slur", Weight: 2 }, { flag: matchedWord, reason: `NSFW word/Curse Word`, Weight: 1 }, { flag: everyonePing, reason: 'Mass pinging', Weight: 2 }, { flag: generalspam, reason: 'Spamming', Weight: 1 }, { flag: duplicateSpam, reason: 'Spamming the same message', Weight: 1 }, { flag: mediaViolation, reason: 'Media violation', Weight: 1 }, { flag: capSpam, reason: 'Spamming Caps', Weight: 1 }];
     let totalWeight = checks.filter(check => check.flag).reduce((acc, check) => { return acc + check.Weight }, 0);
     if (totalWeight == 0) return;
     let reasonText = `AutoMod: ${checks.filter(check => check.flag).map(check => check.reason).join('; ')}`;
+    const { userData } = await getUser({ userId: author.id, guildId: guild.id, modflag: true })
     const isNewUser = Date.now() - member.joinedTimestamp < 2 * 24 * 60 * 60 * 1000 && userData.level < 3
     if (isNewUser) { totalWeight += 1; reasonText += ' while new to the server.'; }
     const commoninputs = { guild: guild, target: member, moderatorUser: client.user, reason: reasonText, channel: channel, isAutomated: true }
