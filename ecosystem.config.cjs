6b51ffa4b2a459e6615e86a30611716737fab1f3
Michael <michaelvargo@outlook.com>
2026-01-21 21:56:53 -0600

    miscellanious commit

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,15 +1,13 @@
 module.exports = {
-  apps: [{
-    name: "Febot",
-    script: "index.js",
-    watch: false,
-    max_memory_restart: "150M",
-    env: {
-      NODE_ENV: "production",
-    },
-    env_production: {
-      NODE_ENV: "production",
-    },
-    exec_mode: "fork"
-  }]
+  apps: [
+    {
+      name: "Febot",
+      script: "interactions.js",
+      watch: false,
+      max_memory_restart: "150M",
+      env: {
+        NODE_ENV: "production",
+      },
+      exec_mode: "fork"
+    }]
 };
