76cd286a47b725cc96a6598236fcf639ab9aa552
Michael <michaelvargo@outlook.com>
2026-05-28 09:41:03 -0500

    use bun native config and lock

--- eslint.config.js
+++
@@ -1,3 +0,0 @@
-import { defineConfig } from 'eslint/config';
-import { configs } from 'typescript-eslint';
-export default defineConfig(configs.recommended, [{ rules: { "@typescript-eslint/no-unused-expressions": 'off', "@typescript-eslint/no-explicit-any": "off", "@typescript-eslint/no-unsafe-assignment": "off", "@typescript-eslint/no-unsafe-member-access": "off", } }]);
